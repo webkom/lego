14ac51f8d593cc1f0dedc8d811e26d56d4929d92
Rolf Erik Lekang <me@rolflekang.com>
2014-09-24 23:05:20 +0200

    Add before launch landing page

---
+++ lego/settings/production.py
@@ -0,0 +1,2 @@
+# -*- coding: utf8 -*-
+DEBUG = False
--- lego/urls.py
+++ lego/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
@@ -8,5 +9,15 @@ urlpatterns = patterns(
     '',
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name="base.html"), name='landing_page'),
 )
+
+if not settings.DEBUG:
+    urlpatterns = patterns(
+        '',
+        url(r'^$', TemplateView.as_view(template_name="before_launch.html"), name='landing_page'),
+    )
+else:
+    urlpatterns = patterns(
+        '',
+        url(r'^$', TemplateView.as_view(template_name="base.html"), name='landing_page'),
+    )
