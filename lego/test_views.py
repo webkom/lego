58be98e9d18cb2b3273af1037efde932d50f111f
Rolf Erik Lekang <me@rolflekang.com>
2014-09-24 21:30:55 +0200

    Use ViewTestCase in SmokeTest

--- lego/test_views.py
+++ lego/test_views.py
@@ -1,12 +1,10 @@
 # -*- coding: utf8 -*-
-
 from django.core.urlresolvers import reverse
-from django.test import TestCase
+
+from lego.utils.test_cases import ViewTestCase
 
 
-class SmokeTest(TestCase):
-    def assertStatusCode(self, response, code=200):
-        self.assertEqual(response.status_code, code)
+class SmokeTest(ViewTestCase):
 
     def test_landing_page(self):
         response = self.client.get(reverse('landing_page'))
