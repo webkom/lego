e9e3ff77e3a4d226032dc6ed654a895b6524c744
Rolf Erik Lekang <me@rolflekang.com>
2014-09-24 19:35:29 +0200

    Add test settings

--- lego/settings/base.py
+++ lego/settings/base.py
@@ -29,12 +29,6 @@ ROOT_URLCONF = 'lego.urls'
 
 WSGI_APPLICATION = 'lego.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 LANGUAGE_CODE = 'en-us'
---
+++ lego/settings/test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake'
+    }
+}
