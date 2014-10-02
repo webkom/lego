987505168773c956a624f63754b1ea0ca0db3310
Rolf Erik Lekang <me@rolflekang.com>
2014-10-02 15:53:38 +0200

    add production settings

--- lego/settings/production.py
+++ lego/settings/production.py
@@ -1,2 +1,36 @@
 # -*- coding: utf8 -*-
 DEBUG = False
+ALLOWED_HOSTS = ['.abakus.no']
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'KEY_PREFIX': 'legoprod'
+    }
+}
+
+with open('/home/webkom/webapps/passwords/lego_db', 'rb') as f:
+    db_password = f.readline()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lego',
+        'USER': 'lego',
+        'PASSWORD': db_password.strip(),
+        'HOST': '129.241.208.149',
+        'PORT': '',
+        'OPTIONS': {
+            'autocommit': True,
+        },
+
+    }
+}
+
+EMAIL_HOST = 'smtp.stud.ntnu.no'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+LOG_PRINT_LEVEL = 1
+
+SESSION_COOKIE_SECURE = True
