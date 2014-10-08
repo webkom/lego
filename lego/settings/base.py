d89933ca010d895312056e1a5c147ebf4749dd22
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2014-10-08 22:52:46 +0200

    Send log events to syslog

--- lego/settings/base.py
+++ lego/settings/base.py
@@ -14,6 +14,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'lego.utils',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -39,3 +41,54 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'syslog': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_false'],
+            'class': 'lego.utils.logger.SyslogHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+        'django.request': {
+            'handlers': ['mail_admins', 'syslog'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.security': {
+            'handlers': ['mail_admins', 'syslog'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'py.warnings': {
+            'handlers': ['console'],
+        },
+        '': {
+            'handlers': ['syslog']
+        }
+    }
+}
