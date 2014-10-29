414ed6af1d422ae86ec2c6b0ecf9afe1a033c371
ekmartin <ekmartinb@gmail.com>
2014-10-29 23:03:19 +0100

    Recreate migrations for users app

--- lego/users/migrations/0001_initial.py
+++ lego/users/migrations/0001_initial.py
@@ -2,10 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import basis.models
+import django.core.validators
 import django.utils.timezone
 from django.conf import settings
-import django.core.validators
+import basis.models
 
 
 class Migration(migrations.Migration):
@@ -18,14 +18,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
                 ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
-                ('username', models.CharField(error_messages={b'unique': 'A user with that username already exists.'}, max_length=30, validators=[django.core.validators.RegexValidator(b'^[\\w.@+-]+$', 'Enter a valid username.  This value may contain only letters, numbers and @/./+/-/_ characters.', b'invalid')], help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', unique=True, verbose_name='username')),
-                ('first_name', models.CharField(max_length=30, verbose_name='first name', blank=True)),
-                ('last_name', models.CharField(max_length=30, verbose_name='last name', blank=True)),
-                ('email', models.EmailField(max_length=75, verbose_name='email address', blank=True)),
+                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', unique=True, validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.  This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], max_length=30, verbose_name='username')),
+                ('first_name', models.CharField(blank=True, max_length=30, verbose_name='first name')),
+                ('last_name', models.CharField(blank=True, max_length=30, verbose_name='last name')),
+                ('email', models.EmailField(blank=True, max_length=75, verbose_name='email address')),
                 ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
@@ -39,10 +39,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='AbakusGroup',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
                 ('name', models.CharField(unique=True, max_length=80, verbose_name='name')),
-                ('parent', models.ForeignKey(verbose_name='parent', blank=True, to='users.AbakusGroup', null=True)),
-                ('permissions', models.ManyToManyField(to='auth.Permission', verbose_name='permissions', blank=True)),
+                ('parent', models.ForeignKey(to='users.AbakusGroup', null=True, blank=True, verbose_name='parent')),
+                ('permissions', models.ManyToManyField(to='auth.Permission', blank=True, verbose_name='permissions')),
             ],
             options={
                 'verbose_name': 'group',
@@ -53,18 +53,18 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Membership',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
                 ('deleted', models.BooleanField(default=False, editable=False)),
                 ('created_at', models.DateTimeField(default=basis.models._now, editable=False)),
                 ('updated_at', models.DateTimeField(default=basis.models._now, editable=False)),
-                ('title', models.CharField(default='Member', max_length=30, verbose_name='role', blank=True)),
+                ('title', models.CharField(default='Member', blank=True, max_length=30, verbose_name='role')),
                 ('start_date', models.DateField(verbose_name='start date')),
-                ('end_date', models.DateField(verbose_name='end date', blank=True)),
-                ('permission_status', models.PositiveSmallIntegerField(default=2, verbose_name='permission status', choices=[(0, 'Previous member'), (1, "Previous member who's still active"), (2, 'Active member'), (3, 'Leader')])),
-                ('created_by', models.ForeignKey(related_name=b'membership_created', default=None, editable=False, to=settings.AUTH_USER_MODEL, null=True)),
-                ('group', models.ForeignKey(verbose_name='group', to='users.AbakusGroup')),
-                ('updated_by', models.ForeignKey(related_name=b'membership_updated', default=None, editable=False, to=settings.AUTH_USER_MODEL, null=True)),
-                ('user', models.ForeignKey(verbose_name='user', to=settings.AUTH_USER_MODEL)),
+                ('end_date', models.DateField(blank=True, verbose_name='end date')),
+                ('permission_status', models.PositiveSmallIntegerField(default=2, choices=[(0, 'Previous member'), (1, "Previous member who's still active"), (2, 'Active member'), (3, 'Leader')], verbose_name='permission status')),
+                ('created_by', models.ForeignKey(default=None, to=settings.AUTH_USER_MODEL, null=True, related_name='membership_created', editable=False)),
+                ('group', models.ForeignKey(to='users.AbakusGroup', verbose_name='group')),
+                ('updated_by', models.ForeignKey(default=None, to=settings.AUTH_USER_MODEL, null=True, related_name='membership_updated', editable=False)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, verbose_name='user')),
             ],
             options={
                 'abstract': False,
@@ -74,13 +74,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='user',
             name='groups',
-            field=models.ManyToManyField(related_query_name=b'user', related_name=b'user_set', to='users.AbakusGroup', through='users.Membership', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups'),
+            field=models.ManyToManyField(related_name='user_set', to='users.AbakusGroup', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', through='users.Membership', blank=True, related_query_name='user', verbose_name='groups'),
             preserve_default=True,
         ),
         migrations.AddField(
             model_name='user',
             name='user_permissions',
-            field=models.ManyToManyField(related_query_name=b'user', related_name=b'user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions'),
+            field=models.ManyToManyField(related_name='user_set', to='auth.Permission', help_text='Specific permissions for this user.', blank=True, related_query_name='user', verbose_name='user permissions'),
             preserve_default=True,
         ),
     ]
