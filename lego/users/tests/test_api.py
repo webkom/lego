5fb90553a98238fe230d167778a07fc95af59c03
ekmartin <ekmartinb@gmail.com>
2014-10-30 21:38:57 +0100

    Start working on the users api. Add tests for listing users

---
+++ lego/users/tests/test_api.py
@@ -0,0 +1,37 @@
+# -*- coding: utf8 -*-
+from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate
+
+from lego.users.models import User
+from lego.users.views.user import UserViewSet
+
+
+class ListUsersAPITestCase(APITestCase):
+    fixtures = ['test_users.yaml', 'test_access_tokens.yaml']
+
+    all_users = User.objects.all()
+    super_user = all_users.filter(is_superuser=True)[0]
+    normal_user = all_users.filter(is_superuser=False)[0]
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.request = self.factory.get('/api/users/')
+        self.view = UserViewSet.as_view({'get': 'list'})
+
+    def test_without_auth(self):
+        response = self.view(self.request)
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data['detail'], 'Authentication credentials were not provided.')
+
+    def test_with_normal_user(self):
+        force_authenticate(self.request, user=self.normal_user)
+        response = self.view(self.request)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_with_super_user(self):
+        force_authenticate(self.request, user=self.super_user)
+        response = self.view(self.request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), len(self.all_users))
---
+++ lego/users/views/user.py
@@ -0,0 +1,36 @@
+# -*- coding: utf8 -*-
+from lego.users.models import User
+from lego.users.serializers import UserSerializer, PublicUserSerializer
+from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions, IsAdminUser
+from rest_framework.response import Response
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, DjangoModelPermissions)
+
+    def list(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            raise PermissionDenied()
+
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+
+        if not request.user.is_superuser:
+            serializer = PublicUserSerializer(user)
+        else:
+            serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+
