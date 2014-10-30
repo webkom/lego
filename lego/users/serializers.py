f15698752fc134723f5abc4a5b78ff27b1674257
ekmartin <ekmartinb@gmail.com>
2014-10-30 21:38:57 +0100

    Start working on the users api. Add tests for listing users

---
+++ lego/users/serializers.py
@@ -0,0 +1,28 @@
+# -*- coding: utf8 -*-
+from lego.users.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'is_staff',
+            'is_active',
+        )
+
+
+class PublicUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'first_name',
+            'last_name'
+        )
