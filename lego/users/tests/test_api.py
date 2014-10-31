6fa48f25653d7c4025495150ff30acbd19a699cb
ekmartin <ekmartinb@gmail.com>
2014-10-31 01:37:15 +0100

    Add tests for creating users

--- lego/users/tests/test_api.py
+++ lego/users/tests/test_api.py
@@ -67,3 +67,50 @@ class GetUsersAPITestCase(APITestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(keys, set(UserSerializer.Meta.fields))
+
+
+class ModifyUsersAPITestCase(APITestCase):
+    fixtures = ['test_users.yaml']
+    super_user = User.objects.filter(is_superuser=True)[0]
+    normal_user = User.objects.filter(is_superuser=False)[0]
+
+    test_user_json = {
+        'username': 'new_testuser',
+        'first_name': 'new',
+        'last_name': 'test_user',
+        'email': 'new@testuser.com',
+    }
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+
+    def test_create_with_normal_user(self):
+        request = self.factory.post('/api/users/', self.test_user_json)
+        force_authenticate(request, user=self.normal_user)
+        view = UserViewSet.as_view({'post': 'create'})
+        response = view(request)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_create_with_super_user(self):
+        request = self.factory.post('/api/users/', self.test_user_json)
+        force_authenticate(request, user=self.super_user)
+        view = UserViewSet.as_view({'post': 'create'})
+        response = view(request)
+
+        self.assertEqual(response.status_code, 201)
+        created_user = User.objects.get(username=self.test_user_json['username'])
+
+        for key, value in self.test_user_json.items():
+            self.assertEqual(getattr(created_user, key), value)
+
+    def test_create_existing_username(self):
+        existing_user = User(**self.test_user_json)
+        existing_user.save()
+
+        request = self.factory.post('/api/users/', self.test_user_json)
+        force_authenticate(request, user=self.super_user)
+        view = UserViewSet.as_view({'post': 'create'})
+        response = view(request)
+
+        self.assertEqual(response.status_code, 400)
