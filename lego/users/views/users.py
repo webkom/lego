119bd8a488133c06bbed8b6ed88377a66518d9ae
ekmartin <ekmartinb@gmail.com>
2014-11-03 14:30:58 +0100

    Fix pep8 errors and fix flake8 conf

--- lego/users/views/users.py
+++ lego/users/views/users.py
@@ -2,7 +2,7 @@
 from rest_framework import viewsets
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from lego.users.models import User
