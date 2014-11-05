8f26bd671cacb7ced3b660400b39d7e117b2986f
Martin Thoresen <martinhath@gmail.com>
2014-11-05 21:25:41 +0100

    Started on rest api for articles

---
+++ lego/app/articles/serializers.py
@@ -0,0 +1,7 @@
+from lego.app.articles.models import Article
+from rest_framework import serializers
+
+class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Article
+        fields = ('title', 'author', 'ingress', 'text', 'can_edit')
