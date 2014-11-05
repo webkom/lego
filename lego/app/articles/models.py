7b6ab664fb847b571f4e1f5a525cf9720b9d4252
Martin Thoresen <martinhath@gmail.com>
2014-11-05 19:12:00 +0100

    Added fields, and made migrations
    Title, author, ingress, text(body), and can_edit fields.

--- lego/app/articles/models.py
+++ lego/app/articles/models.py
@@ -1,6 +1,19 @@
+from basis.models import BasisModel
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
 
-class Article(models.Model):
-    pass
+from lego.users.models import User
+
+
+class Article(BasisModel):
+    title = models.CharField(_('title'), max_length=255)
+    author = models.ForeignKey(User, editable=False, null=True, verbose_name='author')
+
+    ingress = models.TextField(_('ingress'))
+    text = models.TextField(_('article text'), blank=True)
+
+    can_edit = models.ManyToManyField(User, related_name='editable_articles')
+
+    def __str__(self):
+        return self.title
