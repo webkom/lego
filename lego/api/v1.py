a9a0a369944c7c597935a6361b3134858f766f5a
ekmartin <ekmartinb@gmail.com>
2014-11-14 00:29:40 +0100

    Rename flatpages view file to views.py

--- lego/api/v1.py
+++ lego/api/v1.py
@@ -2,7 +2,7 @@
 from rest_framework import routers
 
 from lego.users.views.users import UsersViewSet
-from lego.app.flatpages.api import PageViewSet
+from lego.app.flatpages.views import PageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UsersViewSet)
