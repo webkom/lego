234330cea7a42e11e1432ec0a8322132e18361c3
ekmartin <ekmartinb@gmail.com>
2014-11-15 00:12:59 +0100

    Add a public manager and refactor model

--- lego/app/objectpermissions/models.py
+++ lego/app/objectpermissions/models.py
@@ -4,6 +4,11 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from lego.users.models import User, AbakusGroup
+from lego.app.objectpermissions.managers import PublicObjectPermissionsManager
+
+
+def check_intersection(first, second):
+    return len(set(first).intersection(set(second))) > 0
 
 
 class ObjectPermissionsModel(BasisModel):
@@ -16,14 +21,25 @@ class ObjectPermissionsModel(BasisModel):
 
     require_auth = models.BooleanField(_('require auth'), default=False)
 
+    public_objects = PublicObjectPermissionsManager()
+
     class Meta:
         abstract = True
 
+    def needs_auth(self):
+        return self.require_auth or len(self.can_view_groups.all()) > 0
+
     def can_view(self, user):
+        if not user.is_authenticated():
+            return not self.needs_auth()
+
         return (user == self.created_by
-                or len(set(user.all_groups).intersection(self.can_view_groups.all())) > 0)
+                or check_intersection(user.all_groups, self.can_view_groups.all()))
 
     def can_edit(self, user):
+        if not user.is_authenticated():
+            return not self.needs_auth()
+
         return (user == self.created_by
                 or user in self.can_edit_users.all()
                 or len(set(user.all_groups).intersection(self.can_edit_groups.all())))
