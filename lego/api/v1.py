7b39bc3714ee12c66d0d68a12c6ad4069e388336
Martin Thoresen <martinhath@gmail.com>
2014-11-15 00:08:10 +0100

    Removed auth stuff in articles

--- lego/api/v1.py
+++ lego/api/v1.py
@@ -4,8 +4,10 @@ from rest_framework import routers
 
 from lego.users.views.users import UsersViewSet
 from lego.app.flatpages.views import PageViewSet
+from lego.app.articles.views.articles import ArticlesViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UsersViewSet)
 router.register(r'groups', AbakusGroupViewSet)
 router.register(r'pages', PageViewSet)
+router.register(r'articles', ArticlesViewSet)
--- lego/app/articles/views/articles.py
+++ lego/app/articles/views/articles.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 from rest_framework.response import  Response
 from rest_framework.generics import get_object_or_404
 from rest_framework.exceptions import PermissionDenied
@@ -9,22 +10,20 @@ from lego.app.articles.serializers import ArticleSerializer
 class ArticlesViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = (DjangoModelPermissionsOrAnonReadOnly, )
 
     def list(self, request, *args, **kwargs):
-        def can_view(article):
-            intersect = request.user.groups & article.can_view
-            return  len(intersect) != 0
-        articles = self.queryset.filter(can_view)
+        articles = self.queryset
 
         serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer)
+        return Response(serializer.data)
 
     def retrieve(self, request, pk=None, *args, **kwargs):
         article = get_object_or_404(self.queryset, pk=pk)
         if len(request.user.groups & article.can_view) != 0:
             raise PermissionDenied()
         serializer = ArticleSerializer(article)
-        return Response(serializer)
+        return Response(serializer.data)
 
     def update(self, request, pk=None, *args, **kwargs):
         user = request.user
