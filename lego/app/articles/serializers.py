2c2aa64e78381fa133cf91aaf840acfdc8c8b522
Martin Thoresen <martinhath@gmail.com>
2014-11-15 00:20:26 +0100

    Tests passing

--- lego/app/articles/serializers.py
+++ lego/app/articles/serializers.py
@@ -1,7 +1,7 @@
 from lego.app.articles.models import Article
 from rest_framework import serializers
 
-class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ('title', 'author', 'ingress', 'text')
