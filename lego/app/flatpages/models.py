4acd36238854e1d797d4e50c88f96d03edecaf36
ekmartin <ekmartinb@gmail.com>
2014-11-15 00:14:04 +0100

    Update flatpages to use ObjectPermissions

--- lego/app/flatpages/models.py
+++ lego/app/flatpages/models.py
@@ -1,26 +1,18 @@
 # -*- coding: utf8 -*-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from basis.models import BasisModel
 from markdown import markdown
 
-from .managers import PublicPageManager, InternalPageManager
+from lego.app.objectpermissions.models import ObjectPermissionsModel
 
 
-class Page(BasisModel):
+class Page(ObjectPermissionsModel):
     title = models.CharField(_('title'), max_length=200)
     slug = models.CharField(_('slug'), unique=True, db_index=True, max_length=100)
 
     content = models.TextField(_('content'))
 
     toc = models.BooleanField(default=False, verbose_name=_('Needs table of contents'))
-    require_auth = models.BooleanField(default=False,
-                                       verbose_name=_('Can only be viewed by authenticated users'))
-    require_abakom = models.BooleanField(default=False,
-                                         verbose_name=_('Can only be viewed by abakom users'))
-
-    public_objects = PublicPageManager()
-    internal_objects = InternalPageManager()
 
     class Meta:
         verbose_name = _('flatpage')
@@ -31,8 +23,6 @@ class Page(BasisModel):
         return "%s -- %s" % (self.slug, self.title)
 
     def save(self, *args, **kwargs):
-        if self.require_abakom and not self.require_auth:
-            self.require_auth = True
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
