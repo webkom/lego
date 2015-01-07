50150ed4cd60c6603f7608e9f60d9b95c42a353a
Martin Thoresen <martinhath@gmail.com>
2015-01-07 18:41:48 +0100

    More work on the tests

--- lego/app/articles/views/articles.py
+++ lego/app/articles/views/articles.py
@@ -1,7 +1,7 @@
+from lego.permissions.filters import ObjectPermissionsFilter
+from lego.permissions.model_permissions import PostModelPermissions
+from lego.permissions.object_permissions import ObjectPermissions
 from rest_framework import viewsets
-from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
-from rest_framework.response import  Response
-from rest_framework.generics import get_object_or_404
 
 from lego.app.articles.models import Article
 from lego.app.articles.serializers import ArticleSerializer
@@ -9,17 +9,6 @@ from lego.app.articles.serializers import ArticleSerializer
 class ArticlesViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    permission_classes = (DjangoModelPermissionsOrAnonReadOnly, )
+    filter_backends = (ObjectPermissionsFilter,)
+    permission_classes = (PostModelPermissions, ObjectPermissions)
 
-    def list(self, request, *args, **kwargs):
-        articles = self.queryset
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None, *args, **kwargs):
-        article = get_object_or_404(self.queryset, pk=pk)
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None, *args, **kwargs):
-        return super(ArticlesViewSet, self).update(request, *args, **kwargs)
