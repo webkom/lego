acae906bff511d3a18f60b1393389c58bca3c423
Martin Thoresen <martinhath@gmail.com>
2015-01-16 23:08:11 +0100

    Moved fields from atricles to content

--- lego/app/articles/models.py
+++ lego/app/articles/models.py
@@ -1,16 +1,7 @@
 # -*- coding: utf--8 -*-
-from django.db import models
-
-from lego.users.models import User
-from lego.permissions.models import ObjectPermissionsModel
+from lego.app.content.models import Content
 
 
-class Article(ObjectPermissionsModel):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, editable=False, null=True)
+class Article(Content):
+    pass
 
-    ingress = models.TextField()
-    text = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.title + '(by: {})'.format(self.author)
