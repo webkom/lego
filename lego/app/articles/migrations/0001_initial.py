5eac3af9aef4a4d855787f6f28746f0681ebfb26
Martin Thoresen <martinhath@gmail.com>
2015-01-16 22:58:32 +0100

    Changed article.__str__

--- lego/app/articles/migrations/0001_initial.py
+++ lego/app/articles/migrations/0001_initial.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 import basis.models
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
@@ -17,19 +17,20 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Article',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
-                ('created_at', models.DateTimeField(editable=False, default=basis.models._now)),
-                ('updated_at', models.DateTimeField(editable=False, default=basis.models._now)),
-                ('deleted', models.BooleanField(editable=False, default=False)),
-                ('title', models.CharField(max_length=255, verbose_name='title')),
-                ('ingress', models.TextField(verbose_name='ingress')),
-                ('text', models.TextField(verbose_name='article text', blank=True)),
-                ('author', models.ForeignKey(null=True, to=settings.AUTH_USER_MODEL, editable=False, verbose_name='author')),
-                ('can_view', models.ManyToManyField(to='users.AbakusGroup', related_name='viewable_articles')),
-                ('created_by', models.ForeignKey(related_name='article_created', null=True, to=settings.AUTH_USER_MODEL, editable=False, default=None)),
-                ('groups_can_edit', models.ManyToManyField(to='users.AbakusGroup', related_name='editable_articles')),
-                ('updated_by', models.ForeignKey(related_name='article_updated', null=True, to=settings.AUTH_USER_MODEL, editable=False, default=None)),
-                ('users_can_edit', models.ManyToManyField(to=settings.AUTH_USER_MODEL, related_name='editable_articles')),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('created_at', models.DateTimeField(default=basis.models._now, editable=False)),
+                ('updated_at', models.DateTimeField(default=basis.models._now, editable=False)),
+                ('deleted', models.BooleanField(default=False, editable=False)),
+                ('require_auth', models.BooleanField(default=False, verbose_name='require auth')),
+                ('title', models.CharField(max_length=255)),
+                ('ingress', models.TextField()),
+                ('text', models.TextField(blank=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL, editable=False, null=True)),
+                ('can_edit_groups', models.ManyToManyField(to='users.AbakusGroup', blank=True, null=True, related_name='can_edit_article')),
+                ('can_edit_users', models.ManyToManyField(to=settings.AUTH_USER_MODEL, blank=True, null=True, related_name='can_edit_article')),
+                ('can_view_groups', models.ManyToManyField(to='users.AbakusGroup', blank=True, null=True, related_name='can_view_article')),
+                ('created_by', models.ForeignKey(default=None, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='article_created')),
+                ('updated_by', models.ForeignKey(default=None, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='article_updated')),
             ],
             options={
                 'abstract': False,
--- lego/app/articles/models.py
+++ lego/app/articles/models.py
@@ -13,4 +13,4 @@ class Article(ObjectPermissionsModel):
     text = models.TextField(blank=True)
 
     def __str__(self):
-        return self.title
+        return self.title + '(by: {})'.format(self.author)
