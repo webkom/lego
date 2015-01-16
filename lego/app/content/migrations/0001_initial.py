5930c1914143b5ccdd630f32d9107f1af0443e7b
Martin Thoresen <martinhath@gmail.com>
2015-01-17 00:44:49 +0100

    migrations squash

---
+++ lego/app/content/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='TestMixin',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- lego/app/flatpages/migrations/0001_initial.py
+++ lego/app/flatpages/migrations/0001_initial.py
@@ -9,6 +9,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('users', '0003_auto_20141113_2313'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
@@ -16,23 +17,25 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Page',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('created_at', models.DateTimeField(editable=False, default=basis.models._now)),
                 ('updated_at', models.DateTimeField(editable=False, default=basis.models._now)),
                 ('deleted', models.BooleanField(editable=False, default=False)),
-                ('title', models.CharField(max_length=200, verbose_name='title')),
-                ('slug', models.CharField(verbose_name='slug', db_index=True, unique=True, max_length=100)),
+                ('require_auth', models.BooleanField(verbose_name='require auth', default=False)),
+                ('title', models.CharField(verbose_name='title', max_length=200)),
+                ('slug', models.CharField(verbose_name='slug', unique=True, max_length=100, db_index=True)),
                 ('content', models.TextField(verbose_name='content')),
-                ('toc', models.BooleanField(default=False, verbose_name='Needs table of contents')),
-                ('require_auth', models.BooleanField(default=False, verbose_name='Can only be viewed by authenticated users')),
-                ('require_abakom', models.BooleanField(default=False, verbose_name='Can only be viewed by abakom users')),
-                ('created_by', models.ForeignKey(editable=False, to=settings.AUTH_USER_MODEL, null=True, related_name='page_created', default=None)),
-                ('updated_by', models.ForeignKey(editable=False, to=settings.AUTH_USER_MODEL, null=True, related_name='page_updated', default=None)),
+                ('toc', models.BooleanField(verbose_name='Needs table of contents', default=False)),
+                ('can_edit_groups', models.ManyToManyField(to='users.AbakusGroup', null=True, blank=True, related_name='can_edit_page')),
+                ('can_edit_users', models.ManyToManyField(to=settings.AUTH_USER_MODEL, null=True, blank=True, related_name='can_edit_page')),
+                ('can_view_groups', models.ManyToManyField(to='users.AbakusGroup', null=True, blank=True, related_name='can_view_page')),
+                ('created_by', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='page_created', default=None, editable=False, null=True)),
+                ('updated_by', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='page_updated', default=None, editable=False, null=True)),
             ],
             options={
                 'verbose_name': 'flatpage',
-                'ordering': ('slug',),
                 'verbose_name_plural': 'flatpages',
+                'ordering': ('slug',),
             },
             bases=(models.Model,),
         ),
