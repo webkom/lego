f1e06b525a90a8b7d31c935883470c9dc712734e
Martin Thoresen <martinhath@gmail.com>
2015-01-16 23:05:02 +0100

    Created content app
    Conflicts:
    	lego/app/content/models.py

--- lego/app/content/models.py
+++ lego/app/content/models.py
@@ -1,16 +1,3 @@
-# -*- coding: utf--8 -*-
 from django.db import models
 
-from lego.users.models import User
-from lego.permissions.models import ObjectPermissionsModel
-
-
-class Article(ObjectPermissionsModel):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, editable=False, null=True)
-
-    ingress = models.TextField()
-    text = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.title + '(by: {})'.format(self.author)
+# Create your models here.
