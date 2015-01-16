1525ed8b43c73fa10a835118d61805f5374f46d3
Martin Thoresen <martinhath@gmail.com>
2015-01-16 22:12:05 +0100

    rebase

--- lego/app/articles/models.py
+++ lego/app/articles/models.py
@@ -1,18 +1,16 @@
 # -*- coding: utf--8 -*-
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
-
 
 from lego.users.models import User
 from lego.permissions.models import ObjectPermissionsModel
 
 
 class Article(ObjectPermissionsModel):
-    title = models.CharField(_('title'), max_length=255)
-    author = models.ForeignKey(User, _('author'), editable=False, null=True)
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, editable=False, null=True)
 
-    ingress = models.TextField(_('ingress'))
-    text = models.TextField(_('article text'), blank=True)
+    ingress = models.TextField()
+    text = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
