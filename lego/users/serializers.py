50ac98f496629e97a13fa140352aa7b2d9c81b58
ekmartin <ekmartinb@gmail.com>
2015-01-17 23:30:12 +0100

    Restructure the users API

--- lego/users/serializers.py
+++ lego/users/serializers.py
@@ -2,9 +2,10 @@
 from rest_framework import serializers
 
 from lego.users.models import User
+from lego.users.permissions import can_retrieve_user
 
 
-class UserSerializer(serializers.ModelSerializer):
+class DetailedUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
@@ -27,3 +28,15 @@ class PublicUserSerializer(serializers.ModelSerializer):
             'first_name',
             'last_name'
         )
+
+
+class UserSerializer(DetailedUserSerializer):
+    def to_representation(self, instance):
+        view = self.context['view']
+        request = self.context['request']
+
+        if view.action == 'retrieve' and can_retrieve_user(instance, request.user):
+            serializer = DetailedUserSerializer(instance, context=self.context)
+        else:
+            serializer = PublicUserSerializer(instance, context=self.context)
+        return serializer.data
