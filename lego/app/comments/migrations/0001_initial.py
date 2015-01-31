30f70bd2993594aca6951379eba90470bdf14d08
ekmartin <ekmartinb@gmail.com>
2015-01-31 21:59:00 +0100

    Revert "Merge pull request #43 from webkom/django1.8"
    This reverts commit d154a3e4eefbc457f7bd5bf1015178ea71dc3341, reversing
    changes made to 5a2c0437c0990cfd345f8a7d2cf5757d143804fe.
    
    Recreate comment migration

--- lego/app/comments/migrations/0001_initial.py
+++ lego/app/comments/migrations/0001_initial.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
+        ('contenttypes', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Comment',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
                 ('deleted', models.BooleanField(editable=False, default=False)),
                 ('content', models.TextField()),
                 ('object_id', models.PositiveIntegerField()),
@@ -26,5 +26,6 @@ class Migration(migrations.Migration):
             options={
                 'abstract': False,
             },
+            bases=(models.Model,),
         ),
     ]
