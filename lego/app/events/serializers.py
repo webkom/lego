a470af145656314bccb1b6b9c9ed83484970dc88
Hans-Wilhelm Warlo <hans-wilhelm@warlo.no>
2015-02-02 14:01:38 +0100

    Adds the basic event app functionality. The event model now inherits the Content class, as well as implements the exact same API functionality as the Article app. Creds to martinhath

---
+++ lego/app/events/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from lego.app.events.models import Event
+
+
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Event
+        fields = ('title', 'event_type', 'author', 'text', 'location', 'start_date', 'end_date')
---
+++ lego/app/events/tests/test_events_api.py
@@ -0,0 +1,47 @@
+from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate
+
+from lego.users.models import AbakusGroup, User
+from lego.users.views.users import UsersViewSet
+
+
+class ListEventsTestCase(APITestCase):
+    fixtures = ['initial_abakus_groups.yaml', 'test_events.yaml',
+                'test_users.yaml']
+
+    def setUp(self):
+        self.all_users = User.objects.all()
+
+        self.factory = APIRequestFactory()
+        self.request = self.factory.get('/api/events/')
+        self.view = UsersViewSet.as_view({'get': 'list'})
+
+    def test_with_abakus_user(self):
+        user1 = self.all_users.get(id=3)
+
+        force_authenticate(self.request, user=user1)
+        response = self.view(self.request)
+
+        self.assertEqual(response.status_code, 200)
+
+
+class RetrieveEventsTestCase(APITestCase):
+    fixtures = ['initial_abakus_groups.yaml', 'test_events.yaml',
+                'test_users.yaml']
+
+    def setUp(self):
+        self.all_users = User.objects.all()
+        self.super_user = User.objects.filter(is_superuser=True).first()
+        self.abakus_user = User.objects.filter(is_superuser=False).first()
+
+        self.abakus_group = AbakusGroup.objects.get(name='Abakus')
+        self.abakus_group.add_user(self.abakus_user)
+
+    def test_with_group_permission(self):
+        self.client.force_authenticate(user=self.abakus_user)
+        response = self.client.get('/api/v1/events/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_without_group_permission(self):
+        self.client.force_authenticate(user=self.abakus_user)
+        response = self.client.get('/api/v1/events/2/')
+        self.assertEqual(response.status_code, 404)
