760a254c37ccf82823c6591e8abaef0450bbfb00
Hans-Wilhelm Warlo <hans-wilhelm@warlo.no>
2015-02-03 18:55:02 +0100

    Removes fields from serializer and fixes newline in yaml fixtures.

--- lego/app/events/serializers.py
+++ lego/app/events/serializers.py
@@ -6,5 +6,3 @@ from lego.app.events.models import Event
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ('title', 'event_type', 'author', 'ingress',
-                  'text', 'location', 'start_time', 'end_time')
