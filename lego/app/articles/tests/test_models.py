d5acbbe66d6d1ec5211d162f4ab4666867f2bb13
Martin Thoresen <martinhath@gmail.com>
2015-02-03 19:42:46 +0100

    Moved test_str from Content to Article
    Classes might have different implementations of __str__; for instance,
    it might not make sense for an event to list its author, but for an
    article it does make sense. Since we explicitly check the author in this
    test, we need to make it just for Article.

--- lego/app/articles/tests/test_models.py
+++ lego/app/articles/tests/test_models.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from lego.app.articles.models import Article
 from lego.app.articles.views.articles import ArticlesViewSet
 from lego.app.content.tests import ContentTestMixin
+from lego.users.models import User
 
 
 class ArticleTest(TestCase, ContentTestMixin):
@@ -12,3 +13,9 @@ class ArticleTest(TestCase, ContentTestMixin):
 
     model = Article
     ViewSet = ArticlesViewSet
+
+    def test_str(self):
+        item = self.model.objects.all().get(id=1)
+        author = User.objects.all().get(id=1)
+        self.assertEqual(str(item), item.title +
+                         '(by: ' + str(author) + ')')
