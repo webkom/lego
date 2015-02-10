e7bbb3ced50b8feec0aebed44235203d5e9fb0a5
Martin Thoresen <martinhath@gmail.com>
2015-02-10 20:48:25 +0100

    Fixed created_by on articles

--- lego/app/articles/serializers.py
+++ lego/app/articles/serializers.py
@@ -7,3 +7,13 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ('title', 'author', 'ingress', 'text')
+
+    def create(self, validated_data):
+        request = self.context['request']
+        article = Article.objects.create(created_by=request.user, **validated_data)
+        return article
+
+    def update(self, instance, validated_data):
+        request = self.context['request']
+        instance.updated_by = request.user
+        return instance
