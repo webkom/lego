c8fefb258fe4ff7507249590f0aad24fb5c53621
Martin Thoresen <martinhath@gmail.com>
2015-02-10 21:09:38 +0100

    Moved the base seralizer to its own class
    Where should it be?

--- lego/app/articles/serializers.py
+++ lego/app/articles/serializers.py
@@ -1,19 +1,9 @@
-from rest_framework import serializers
+from lego.halla import BaseSerializer
 
 from lego.app.articles.models import Article
 
 
-class ArticleSerializer(serializers.ModelSerializer):
+class ArticleSerializer(BaseSerializer):
     class Meta:
         model = Article
         fields = ('title', 'author', 'ingress', 'text')
-
-    def create(self, validated_data):
-        request = self.context['request']
-        article = Article.objects.create(created_by=request.user, **validated_data)
-        return article
-
-    def update(self, instance, validated_data):
-        request = self.context['request']
-        instance.updated_by = request.user
-        return instance
