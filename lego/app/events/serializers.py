1518a313dc8115e391f00417a5a52014692f34e6
Hans-Wilhelm Warlo <hans-wilhelm@warlo.no>
2015-02-18 23:09:42 +0100

    Changes merge_date to merge_time, migrates it. In addition total_capacity_count is now appended to the fields in the EventSerializer

--- lego/app/events/serializers.py
+++ lego/app/events/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from basis.serializers import BasisSerializer
 
 from lego.app.events.models import Event, Pool
@@ -12,10 +13,13 @@ class PoolSerializer(BasisSerializer):
 
 class EventSerializer(BasisSerializer):
 
-    pools = PoolSerializer(many=True, required=False)
+    pools = PoolSerializer(many=True)
+    total_capacity_count = serializers.ReadOnlyField()
 
     class Meta:
         model = Event
+        fields = ('title', 'author', 'ingress', 'text', 'event_type', 'location',
+                  'start_time', 'end_time', 'merge_time', 'pools', 'total_capacity_count')
 
     def create(self, validated_data):
         pool_data = validated_data.pop('pools')
