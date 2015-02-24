6d27a18de2a814b8e321a2b9a090356034c0dd05
Andreas Broby <and.broby@gmail.com>
2015-02-24 19:42:13 +0100

    descriptive argument names

--- lego/app/events/models.py
+++ lego/app/events/models.py
@@ -46,8 +46,8 @@ class Event(Content, BasisModel, ObjectPermissionsModel):
     def slug(self):
         return slugify(self.title)
 
-    def add_pool(self, name, size, date):
-        return self.pools.create(name=name, size=size, activation_date=date)
+    def add_pool(self, name, size, activation_date):
+        return self.pools.create(name=name, size=size, activation_date=activation_date)
 
     def can_register(self, user=None, pool=None):
         """
