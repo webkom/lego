25dfb80e52b95041a31abad1bed53e4c267b9332
Andreas Broby <and.broby@gmail.com>
2015-03-12 13:15:46 +0100

    forgot to commit the changes to views when moving to using two serializers

--- lego/app/events/views/events.py
+++ lego/app/events/views/events.py
@@ -1,14 +1,20 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from lego.app.events.models import Event
-from lego.app.events.serializers import EventSerializer
+from lego.app.events.serializers import *
 from lego.permissions.filters import ObjectPermissionsFilter
 from lego.permissions.model_permissions import PostModelPermissions
 from lego.permissions.object_permissions import ObjectPermissions
 
 
 class EventViewSet(viewsets.ModelViewSet):
+    def get_serializer_class(self):
+        if self.action == 'create' or self.action == 'update':
+            return EventCreateAndUpdateSerializer
+        return EventReadSerializer
+
     queryset = Event.objects.all()
-    serializer_class = EventSerializer
     filter_backends = (ObjectPermissionsFilter,)
     permission_classes = (PostModelPermissions, ObjectPermissions)
