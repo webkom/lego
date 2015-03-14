8bd3369dd58f99494db993b6017e79d1b3f35dd9
Andreas Broby <and.broby@gmail.com>
2015-03-14 02:48:22 +0100

    misc stuff

---
+++ lego/app/events/migrations/0008_auto_20150311_0255.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0007_registration_event'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='registration',
+            old_name='waiting_pool',
+            new_name='waiting_pools',
+        ),
+    ]
