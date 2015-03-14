72b5eacb44d28264288bc9aceff4230a8726065f
Andreas Broby <and.broby@gmail.com>
2015-03-14 02:53:49 +0100

    lots of migrations

---
+++ lego/app/events/migrations/0011_auto_20150314_0128.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0010_auto_20150311_0307'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='pool',
+            old_name='size',
+            new_name='capacity',
+        ),
+    ]
