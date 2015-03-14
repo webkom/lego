e01b438190981b7a22739b1aeafad1a235bfda07
Andreas Broby <and.broby@gmail.com>
2015-03-14 01:40:00 +0100

    just test updates ~~

--- lego/app/events/tests/test_models.py
+++ lego/app/events/tests/test_models.py
@@ -92,35 +92,26 @@ class RegistrationTestCase(TestCase):
         pool = event.pools.first()
         people_to_place_in_waiting_list = 3
 
-        for n in range(pool.size + 3):
-            username = first_name = last_name = email = str(n)
-            user = User(username=username, first_name=first_name, last_name=last_name, email=email)
-            user.save()
+        users = self.get_dummy_users(pool.size + 3)
+        for user in users:
             event.register(user=user, pool=pool)
 
         self.assertEqual(event.number_of_waiting_registrations, people_to_place_in_waiting_list)
 
-
-    @unittest.expectedFailure
-    def test_unable_to_register_if_full(self):
-        event = Event.objects.get(pk=1)
-        pool = event.add_pool("1-5 klasse", 1, timezone.now() - timedelta(hours=24))
-        event.register(user=None, pool=pool)
-        self.assertRaises(EventFullException, event.register, None, pool)
-        registrations = pool.number_of_registrations
-        self.assertEqual(registrations, event.number_of_registrations)
-
-    @unittest.expectedFailure
     def test_can_register_pre_merge(self):
-        event = Event.objects.get(pk=1)
-        pool_one = event.add_pool("1-2 klasse", 1, timezone.now() - timedelta(hours=24))
-        pool_two = event.add_pool("3-5 klasse", 1, timezone.now() - timedelta(hours=24))
-        event.register(user=None, pool=pool_one)
-        capacity = pool_one.number_of_registrations
-        self.assertEqual(pool_one.size, event.number_of_registrations)
-        event.register(user=None, pool=pool_two)
-        capacity += pool_two.number_of_registrations
-        self.assertEqual(capacity, event.number_of_registrations)
+        event = Event.objects.get(title="POOLS")
+        pool_one = event.pools.first()
+        pool_two = event.pools.get(pk=2)
+        users = self.get_dummy_users(2)
+        user_one, user_two = users[0], users[1]
+
+        event.register(user=user_one, pool=pool_one)
+        n_registrants = pool_one.number_of_registrations
+        self.assertEqual(pool_one.number_of_registrations, event.number_of_registrations)
+
+        event.register(user=user_two, pool=pool_two)
+        n_registrants += pool_two.number_of_registrations
+        self.assertEqual(n_registrants, event.number_of_registrations)
 
     @unittest.expectedFailure
     def test_can_register_post_merge(self):
