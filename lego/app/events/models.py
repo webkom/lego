8a0dc3a27e6407ed3c98900c2cd8dddb740e22a8
Andreas Broby <and.broby@gmail.com>
2015-03-14 05:26:43 +0100

    wrote a bunch of tests

--- lego/app/events/models.py
+++ lego/app/events/models.py
@@ -88,7 +88,8 @@ class Event(Content, BasisModel, ObjectPermissionsModel):
         self.notify_unregistration(pool_unregistered_from=pool)
 
     def notify_unregistration(self, pool_unregistered_from):
-        if self.number_of_registrations < self.capacity:
+        if (self.number_of_registrations < self.capacity and
+                pool_unregistered_from is not None):
             if self.is_merged:
                 self.bump()
             else:
@@ -129,7 +130,7 @@ class Event(Content, BasisModel, ObjectPermissionsModel):
         """
         Calculates total registrations with or without multiple pools.
         """
-        return self.registrations.filter(waiting_list=None).count()
+        return self.registrations.filter(waiting_list=None, waiting_pool=None).count()
 
     @property
     def number_of_waiting_registrations(self):
--- lego/app/events/tests/test_models.py
+++ lego/app/events/tests/test_models.py
@@ -5,9 +5,9 @@ from django.utils import timezone
 from django.utils.text import slugify
 
 from lego.app.content.tests import ContentTestMixin
-from lego.users.models import User
 from lego.app.events.models import Event
 from lego.app.events.views.events import EventViewSet
+from lego.users.models import User
 
 
 class EventTest(TestCase, ContentTestMixin):
@@ -75,13 +75,14 @@ class RegistrationTestCase(TestCase):
     def test_waiting_list_if_full(self):
         event = Event.objects.get(title="POOLS")
         pool = event.pools.first()
-        people_to_place_in_waiting_list = 3
+        people_2_place_in_waiting_list = 3
 
-        users = self.get_dummy_users(pool.capacity + 3)
+        users = self.get_dummy_users(pool.capacity + people_2_place_in_waiting_list)
         for user in users:
             event.register(user=user, pool=pool)
 
-        self.assertEqual(event.waiting_list.number_of_registrations, people_to_place_in_waiting_list)
+        self.assertEqual(event.waiting_list.number_of_registrations, people_2_place_in_waiting_list)
+        self.assertEqual(pool.number_of_registrations, pool.capacity)
         self.assertEqual(event.number_of_registrations, pool.number_of_registrations)
 
     def test_number_of_waiting_registrations(self):
@@ -160,7 +161,121 @@ class RegistrationTestCase(TestCase):
         pool = event.pools.first()
         user = self.get_dummy_users(1)[0]
 
-        event.register(user=user, pool=pool)
-        registrations_before = event.number_of_registrations
-        event.unregister(user)
+        with AssertInvariant(event.waiting_list):
+            event.register(user=user, pool=pool)
+            registrations_before = event.number_of_registrations
+            event.unregister(user)
+
         self.assertEqual(event.number_of_registrations, registrations_before-1)
+
+    def test_popping_from_waiting_list_pre_merge(self):
+        event = Event.objects.get(title="NO_POOLS")
+        pool = event.add_pool("3-5 klasse", 0, timezone.now() - timedelta(hours=24))
+        users = self.get_dummy_users(pool.capacity + 10)
+        for user in users:
+            event.register(user=user, pool=pool)
+        prev = event.waiting_list.pop()
+        while event.waiting_list.registrations.count() > 0:
+            top = event.waiting_list.pop()
+            self.assertLess(prev.registration_date, top.registration_date)
+
+    def test_popping_from_waiting_list_post_merge(self):
+        event = Event.objects.get(title="NO_POOLS")
+        pool = event.add_pool("3-5 klasse", 0, timezone.now() - timedelta(hours=24))
+        users = self.get_dummy_users(pool.capacity + 10)
+        for user in users:
+            event.register(user=user, pool=pool)
+
+        prev = event.waiting_list.pop()
+        while event.waiting_list.registrations.count() > 0:
+            top = event.waiting_list.pop()
+            self.assertLess(prev.registration_date, top.registration_date)
+
+    def test_unregistering_from_waiting_list(self):
+        event = Event.objects.get(title="POOLS")
+        pool = event.pools.first()
+        users = self.get_dummy_users(pool.capacity + 10)
+        for user in users:
+            event.register(user=user, pool=pool)
+
+        event_size_before = event.number_of_registrations
+        pool_size_before = pool.number_of_registrations
+        waiting_list_before = event.number_of_waiting_registrations
+
+        with AssertInvariant(event.waiting_list):
+            event.unregister(users[-1])
+
+        self.assertEqual(event.number_of_registrations, event_size_before)
+        self.assertEqual(pool.number_of_registrations, pool_size_before)
+        self.assertEqual(event.number_of_waiting_registrations, waiting_list_before-1)
+        self.assertLessEqual(event.number_of_registrations, event.capacity)
+
+    def test_unregistering_and_bumping(self):
+        event = Event.objects.get(title="POOLS")
+        pool = event.pools.first()
+        users = self.get_dummy_users(pool.capacity + 10)
+        for user in users:
+            event.register(user=user, pool=pool)
+
+        waiting_list_before = event.number_of_waiting_registrations
+        event_size_before = event.number_of_registrations
+        pool_size_before = pool.number_of_registrations
+
+        with AssertInvariant(event.waiting_list):
+            user_to_unregister = event.registrations.first().user
+            event.unregister(user_to_unregister)
+
+        self.assertEqual(pool.number_of_registrations, pool_size_before)
+        self.assertEqual(event.number_of_registrations, event_size_before)
+        self.assertEqual(event.number_of_waiting_registrations, waiting_list_before-1)
+        self.assertLessEqual(event.number_of_registrations, event.capacity)
+
+    def test_unregistering_and_bumping_post_merge(self):
+        event = Event.objects.get(title="NO_POOLS")
+        event.merge_time = timezone.now() - timedelta(hours=24)
+        pool_one = event.add_pool("1-2 klasse", 1, timezone.now() - timedelta(hours=24))
+        pool_two = event.add_pool("2-3 klasse", 1, timezone.now() - timedelta(hours=24))
+        users = self.get_dummy_users(3)
+        event.register(users.pop(), pool=pool_two)
+        for user in users:
+            event.register(user=user, pool=pool_one)
+
+        waiting_list_before = event.number_of_waiting_registrations
+        event_size_before = event.number_of_registrations
+        pool_one_size_before = pool_one.number_of_registrations
+        pool_two_size_before = pool_two.number_of_registrations
+
+        user_to_unregister = pool_two.registrations.first().user
+
+        with AssertInvariant(event.waiting_list):
+            event.unregister(user_to_unregister)
+
+        self.assertEqual(event.number_of_registrations, event_size_before)
+        self.assertEqual(event.number_of_waiting_registrations, waiting_list_before-1)
+        self.assertEqual(pool_one.number_of_registrations, pool_one_size_before + 1)
+        self.assertGreater(pool_one.number_of_registrations, pool_one.capacity)
+        self.assertEqual(pool_two.number_of_registrations, pool_two_size_before - 1)
+        self.assertLessEqual(event.number_of_registrations, event.capacity)
+
+
+class AssertInvariant:
+    def __init__(self, waiting_list):
+        self.registrations = waiting_list.registrations
+
+    def assertInvariant(self):
+        elements = self.registrations.all()
+        if len(elements[1:]) > 1:
+            prev = elements[0]
+            for registration in elements[1:]:
+                if prev.registration_date > registration.registration_date:
+                    raise self.InvariantViolation()
+                prev = registration
+
+    def __enter__(self):
+        self.assertInvariant()
+
+    def __exit__(self, type, value, traceback):
+        self.assertInvariant()
+
+    class InvariantViolation(Exception):
+        pass
