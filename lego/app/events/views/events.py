093c373a0831b58bdbac2c04a5ef64a528ee7171
Andreas Broby <and.broby@gmail.com>
2015-03-14 05:42:17 +0100

    if only guido spent as much time working on the language as he does forcing his coding styles on others

--- lego/app/events/views/events.py
+++ lego/app/events/views/events.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from lego.app.events.models import Event
 from lego.app.events.serializers import *
