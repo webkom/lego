34c9b00ee12fe33d77b892563b7ca1109e633502
ekmartin <ekmartinb@gmail.com>
2015-03-17 20:25:56 +0100

    Add permissions documentation

--- lego/permissions/filters.py
+++ lego/permissions/filters.py
@@ -4,6 +4,9 @@ from rest_framework import filters
 
 
 class ObjectPermissionsFilter(filters.BaseFilterBackend):
+    """
+    A filter backend for DRF-views that want to be able to filter based on object permissions.
+    """
     def filter_queryset(self, request, queryset, view):
         if not request.user.is_authenticated():
             return queryset.filter(require_auth=False, can_view_groups=None)
