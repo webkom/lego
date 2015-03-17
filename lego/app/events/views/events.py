9a690305c2d933b607d3bc09daf150c0bbe5a18b
Hans-Wilhelm Warlo <hans-wilhelm@warlo.no>
2015-03-17 20:28:24 +0100

    Adds detail route for creation of pool after event is created.

--- lego/app/events/views/events.py
+++ lego/app/events/views/events.py
@@ -1,8 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 
 from lego.app.events.models import Event
 from lego.app.events.permissions import EventPermissions
-from lego.app.events.serializers import EventCreateAndUpdateSerializer, EventReadSerializer
+from lego.app.events.serializers import (EventCreateAndUpdateSerializer, EventReadSerializer,
+                                         PoolSerializer)
 from lego.permissions.filters import ObjectPermissionsFilter
 
 
@@ -15,3 +18,14 @@ class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     filter_backends = (ObjectPermissionsFilter,)
     permission_classes = (EventPermissions,)
+
+    @detail_route(methods=['POST'], url_path='pools')
+    def add_pool(self, request, pk=None):
+        event = self.get_object()
+        serializer = PoolSerializer(data=request.data)
+        if serializer.is_valid():
+            event.add_pool(serializer.data['name'], serializer.data['capacity'],
+                           serializer.data['activation_date'])
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
