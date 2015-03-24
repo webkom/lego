4691098b6dddedee8da27329092ae0387f33d558
ekmartin <ekmartinb@gmail.com>
2015-03-24 22:17:39 +0100

    Add JWT tests

--- lego/api/urls.py
+++ lego/api/urls.py
@@ -22,7 +22,8 @@ def version_redirect(request, path):
 urlpatterns = patterns(
     '',
     url(r'^v1/', include(v1.urls)),
-    url(r'token-auth/$', 'rest_framework_jwt.views.obtain_jwt_token'),
-    url(r'token-auth/refresh/$', 'rest_framework_jwt.views.refresh_jwt_token'),
+    url(r'token-auth/$', 'rest_framework_jwt.views.obtain_jwt_token', name='obtain_jwt_token'),
+    url(r'token-auth/refresh/$', 'rest_framework_jwt.views.refresh_jwt_token',
+        name='refresh_jwt_token'),
     url(r'^(.*)/$', version_redirect),
 )
