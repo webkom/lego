4fe99353744331ee8c17dd499998e69c6c89cadd
ekmartin <mail@ekmartin.no>
2015-08-26 22:34:20 +0200

    Add postgresql as a frigg service

--- lego/settings/test.py
+++ lego/settings/test.py
@@ -6,11 +6,8 @@ TESTING = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lego',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': '127.0.0.1',
+        'NAME': 'lego'
     }
 }
 
