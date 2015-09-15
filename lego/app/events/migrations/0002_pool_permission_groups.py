93d7be097425e1279eff741770d2ad9e08408b98
Hans-Wilhelm Warlo <hw@warlo.no>
2015-09-15 21:59:02 +0200

    Adds permission_groups m2m field to pools and makes use of the permission group system for registration to events using pools.

---
+++ lego/app/events/migrations/0002_pool_permission_groups.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0002_auto_20150826_2000'),
+        ('events', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='pool',
+            name='permission_groups',
+            field=models.ManyToManyField(to='users.AbakusGroup'),
+        ),
+    ]
