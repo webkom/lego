4a71def371bbacc80343654a3b24f3e861c85b7b
Hans-Wilhelm Warlo <hw@warlo.no>
2015-09-25 11:55:19 +0200

    Adds slugfield in the abstract content model, and removes the slug code from events. Updates existing fixtures with a wrong slug, and adds a test that checks that the slug saves correctly.

--- lego/app/content/models.py
+++ lego/app/content/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf--8 -*-
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+from django.utils.text import slugify
 
 from lego.app.comments.models import Comment
 from lego.users.models import User
@@ -15,6 +16,11 @@ class Content(models.Model):
     description = models.TextField()
     text = models.TextField(blank=True)
     comments = GenericRelation(Comment)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify('%s-%s' % (self.id, self.title))
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title + '(by: {})'.format(self.author)
