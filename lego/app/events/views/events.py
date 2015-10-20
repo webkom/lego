250f3874f06afaefa09d817f583ac8dc219874ed
Hans-Wilhelm Warlo <hw@warlo.no>
2015-10-20 15:33:21 +0200

    Adds working permissions on pools

--- lego/app/events/views/events.py
+++ lego/app/events/views/events.py
@@ -1,32 +1,29 @@
 from rest_framework import viewsets
 
 from lego.app.events.models import Event, Pool
-from lego.app.events.permissions import EventPermissions
+from lego.app.events.permissions import EventPermissions, NestedEventPermissions
 from lego.app.events.serializers import (EventCreateAndUpdateSerializer, EventReadSerializer,
                                          PoolSerializer)
 from lego.permissions.filters import ObjectPermissionsFilter
 
 
 class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    filter_backends = (ObjectPermissionsFilter,)
+    permission_classes = (EventPermissions,)
+
     def get_serializer_class(self):
         if self.action == 'create' or self.action == 'update':
             return EventCreateAndUpdateSerializer
         return EventReadSerializer
 
-    queryset = Event.objects.all()
-    filter_backends = (ObjectPermissionsFilter,)
-    permission_classes = (EventPermissions,)
-
 
 class PoolViewSet(viewsets.ModelViewSet):
-
     serializer_class = PoolSerializer
     queryset = Pool.objects.all()
-    # filter_backends = (ObjectPermissionsFilter,)
-    permission_classes = (EventPermissions,)
+    permission_classes = (NestedEventPermissions,)
 
     def get_queryset(self):
         event_id = self.kwargs.get('event_pk', None)
         if event_id:
             return Pool.objects.filter(event=event_id)
-        return super(PoolViewSet, self).get_queryset()
