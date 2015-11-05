d3acb33f3692d57401df2ed6be27fabf82cd18f4
Marius Kotlarz <marius.kotlarz@gmail.com>
2015-11-05 22:04:53 +0100

    Fixed query_params for Quotes and removed duplicate code

--- lego/app/quotes/views/quotes.py
+++ lego/app/quotes/views/quotes.py
@@ -30,25 +30,12 @@ class QuoteViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, QuotePermissions)
 
     def get_queryset(self):
-        approved = self.request.query_params.get('approved') or True
-        if not approved:
+        approved = self.request.query_params.get('approved')
+        if approved is not None and approved != '' and approved == 'false':
             approved = not self.request.user.has_perm(QuotePermissions.perms_map['approve'])
-
+        else:
+            approved = True
         return Quote.objects.filter(approved=approved)
-
-    @list_route()
-    def unapproved(self, request):
-        if not self.request.user.has_perm(QuotePermissions.perms_map['approve']):
-            raise PermissionDenied()
-        quotes = Quote.objects.filter(approved=False)
-        serialized_quotes = []
-        for instance in quotes:
-            serializer = QuoteUnapprovedReadSerializer(instance, context={'request': request})
-            serialized_quotes.append(serializer.data)
-        return Response(
-            serialized_quotes,
-            status=status.HTTP_200_OK
-        )
 
     def retrieve(self, request, pk=None):
         instance = self.get_object(pk)
@@ -66,26 +53,31 @@ class QuoteViewSet(viewsets.ModelViewSet):
         }
         serializer = QuoteLikeSerializer(data=data)
         if serializer.is_valid():
-            instance = self.get_object(pk)
-            if not instance.is_approved():
-                raise PermissionDenied()
-            instance.like(user=request.user)
-            # TODO: different serializer?
-            serializer = QuoteApprovedReadSerializer(instance, context={'request': request})
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
+            return self._likeQuote(request=request, pk=pk)
         else:
             # TODO: Not found 404?
             raise ValidationError(serializer.errors)
 
     @detail_route(methods=['POST'], url_path='unlike')
     def unlike(self, request, pk=None):
+        return self._likeQuote(request=request, pk=pk, like=False)
+
+    @detail_route(methods=['PUT'], url_path='approve')
+    def approve(self, request, pk=None):
+        return self._approveQuote(request=request, pk=pk)
+
+    @detail_route(methods=['PUT'], url_path='unapprove')
+    def unapprove(self, request, pk=None):
+        return self._approveQuote(request=request, pk=pk, approve=False)
+
+    def _likeQuote(self, request, pk, like=True):
         instance = self.get_object(pk)
         if not instance.is_approved():
             raise PermissionDenied()
-        result = instance.unlike(user=request.user)
+        if like:
+            result = instance.like(user=request.user)
+        else:
+            result = instance.unlike(user=request.user)
         # TODO: do something with result?
         # TODO: different serializer?
         serializer = QuoteApprovedReadSerializer(instance, context={'request': request})
@@ -94,28 +86,15 @@ class QuoteViewSet(viewsets.ModelViewSet):
             status=status.HTTP_201_CREATED
         )
 
-    @detail_route(methods=['PUT'], url_path='approve')
-    def approve(self, request, pk=None):
+    def _approveQuote(self, request, pk, approve=True):
         if not self.request.user.has_perm(QuotePermissions.perms_map['approve']):
             raise PermissionDenied()
         instance = self.get_object(pk)
-        result = instance.approve()
-        # TODO: do something with result?
-        # TODO: different serializer?
-        serializer = QuoteApprovedReadSerializer(instance, context={'request': request})
-        return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-        )
-
-    @detail_route(methods=['PUT'], url_path='unapprove')
-    def unapprove(self, request, pk=None):
-        if not self.request.user.has_perm(QuotePermissions.perms_map['approve']):
-            raise PermissionDenied()
-        instance = self.get_object(pk)
-        result = instance.unapprove()
-        # TODO: different serializer?
-        # TODO: do something with result?
+        if approve:
+            result = instance.approve()
+        else:
+            result = instance.unapprove()
+        # TODO: do something with result as it returns True/False if it succeeds/fails.
         serializer = QuoteApprovedReadSerializer(instance, context={'request': request})
         return Response(
             serializer.data,
