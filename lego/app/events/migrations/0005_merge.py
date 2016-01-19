3bd378f8ae53b95665484b2a2ddb92187eb7465e
Hans-Wilhelm Warlo <hw@warlo.no>
2016-01-19 19:58:03 +0100

    Fix isort and migration

---
+++ lego/app/events/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0002_auto_20160119_1808'),
+        ('events', '0004_auto_20151106_2130'),
+    ]
+
+    operations = [
+    ]
