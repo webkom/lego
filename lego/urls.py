54b9e6b6aab23d2ebd055e298e1a16e12605fae4
ekmartin <mail@ekmartin.com>
2016-01-19 22:32:52 +0100

    Upgrade to Django 1.9

--- lego/urls.py
+++ lego/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     url(r'^api/', include('lego.api.urls')),
 
-    url(r'^authorization/token-auth/$', 'rest_framework_jwt.views.obtain_jwt_token',
+    url(r'^authorization/token-auth/$', obtain_jwt_token,
         name='obtain_jwt_token'),
-    url(r'^authorization/token-auth/refresh/$', 'rest_framework_jwt.views.refresh_jwt_token',
+    url(r'^authorization/token-auth/refresh/$', refresh_jwt_token,
         name='refresh_jwt_token'),
     url(r'^authorization/oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^authorization/', include('rest_framework.urls', namespace='rest_framework')),
