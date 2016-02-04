33c4ae90f821733531eb02a2036d00a87f917208
Hans-Wilhelm Warlo <hw@warlo.no>
2016-02-04 18:47:36 +0100

    Adds detailed event serializer. Detailed endpoint will now also return registered users, while listing wont.

--- lego/app/events/serializers.py
+++ lego/app/events/serializers.py
@@ -6,7 +6,58 @@ from lego.app.comments.serializers import CommentSerializer
 from lego.app.events.models import Event, Pool, Registration
 
 
-class PoolSerializer(BasisSerializer):
+class RegistrationReadSerializer(BasisSerializer):
+
+    class Meta:
+        model = Registration
+        fields = ('id', 'user')
+
+
+class PoolReadSerializer(BasisSerializer):
+    registrations = RegistrationReadSerializer(many=True)
+
+    class Meta:
+        model = Pool
+        fields = ('id', 'name', 'capacity', 'activation_date', 'permission_groups', 'registrations')
+
+    def create(self, validated_data):
+        event = Event.objects.get(pk=self.context['view'].kwargs['event_pk'])
+        permission_groups = validated_data.pop('permission_groups')
+        pool = Pool.objects.create(event=event, **validated_data)
+
+        if permission_groups:
+            for group in permission_groups:
+                pool.permission_groups.add(group)
+
+        return pool
+
+
+class EventReadSerializer(BasisSerializer):
+    comments = CommentSerializer(read_only=True, many=True)
+    comment_target = CharField(read_only=True)
+    capacity = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Event
+        fields = ('id', 'title', 'author', 'description', 'text', 'event_type', 'location',
+                  'comments', 'comment_target', 'start_time', 'end_time', 'capacity')
+
+
+class EventReadDetailedSerializer(BasisSerializer):
+    comments = CommentSerializer(read_only=True, many=True)
+    comment_target = CharField(read_only=True)
+    pools = PoolReadSerializer(many=True)
+    capacity = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Event
+        fields = ('id', 'title', 'author', 'description', 'text', 'event_type', 'location',
+                  'comments', 'comment_target', 'start_time', 'end_time', 'pools',
+                  'capacity', 'waiting_list')
+
+
+class PoolCreateAndUpdateSerializer(BasisSerializer):
+
     class Meta:
         model = Pool
         fields = ('id', 'name', 'capacity', 'activation_date', 'permission_groups')
@@ -28,19 +79,6 @@ class EventCreateAndUpdateSerializer(BasisSerializer):
         model = Event
         fields = ('id', 'title', 'author', 'description', 'text', 'event_type', 'location',
                   'start_time', 'end_time', 'merge_time')
-
-
-class EventReadSerializer(BasisSerializer):
-    comments = CommentSerializer(read_only=True, many=True)
-    comment_target = CharField(read_only=True)
-    pools = PoolSerializer(many=True)
-    capacity = serializers.ReadOnlyField()
-
-    class Meta:
-        model = Event
-        fields = ('id', 'title', 'author', 'description', 'text', 'event_type', 'location',
-                  'comments', 'comment_target', 'start_time', 'end_time', 'pools',
-                  'capacity', 'waiting_list')
 
 
 class RegistrationCreateAndUpdateSerializer(BasisSerializer):
