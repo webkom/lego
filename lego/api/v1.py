0e32117f4ab110d3145d211cb02615cd00760e64
Hans-Wilhelm Warlo <hw@warlo.no>
2016-02-11 20:21:56 +0100

    Removes registration endpoint, registrations are contained in detailed event. Updates tests as well

--- lego/api/v1.py
+++ lego/api/v1.py
@@ -15,7 +15,5 @@ router.register(r'articles', ArticlesViewSet)
 router.register(r'comments', CommentViewSet)
 router.register(r'events', EventViewSet)
 router.register(r'events/(?P<event_pk>[^/]+)/pools', PoolViewSet)
-router.register(r'events/(?P<event_pk>[^/]+)/pools/(?P<pool_pk>[^/]+)/registrations',
-                RegistrationViewSet)
 router.register(r'events/(?P<event_pk>[^/]+)/register',
                 RegistrationViewSet, base_name='register')
