90012049a4d72a8b2fdf1d9d927b61cc78c171dd
Hans-Wilhelm Warlo <hw@warlo.no>
2016-03-04 01:33:44 +0100

    Adds helper function in test

--- lego/app/events/tests/test_models.py
+++ lego/app/events/tests/test_models.py
@@ -72,30 +72,23 @@ class RegistrationMethodTest(TestCase):
 class PoolCapacityTestCase(TestCase):
     fixtures = ['initial_abakus_groups.yaml', 'test_users.yaml', 'test_events.yaml']
 
-    def test_capacity_with_single_pool(self):
-        event = Event.objects.get(title='NO_POOLS_ABAKUS')
-        capacities_to_add = [10]
-        permission_groups = [AbakusGroup.objects.get(name='Abakus')]
-
+    def create_pools(self, event, capacities_to_add, permission_groups):
         for capacity in capacities_to_add:
             pool = Pool.objects.create(
                 name='Abakus', capacity=capacity, event=event,
                 activation_date=(timezone.now() - timedelta(hours=24)))
             pool.permission_groups = permission_groups
 
+    def test_capacity_with_single_pool(self):
+        event = Event.objects.get(title='NO_POOLS_ABAKUS')
+        capacities_to_add = [10]
+        self.create_pools(event, capacities_to_add, [AbakusGroup.objects.get(name='Abakus')])
         self.assertEqual(sum(capacities_to_add), event.capacity)
 
     def test_capacity_with_multiple_pools(self):
         event = Event.objects.get(title='NO_POOLS_ABAKUS')
         capacities_to_add = [10, 20]
-        permission_groups = [AbakusGroup.objects.get(name='Abakus')]
-
-        for capacity in capacities_to_add:
-            pool = Pool.objects.create(
-                name='Abakus', capacity=capacity, event=event,
-                activation_date=(timezone.now() - timedelta(hours=24)))
-            pool.permission_groups = permission_groups
-
+        self.create_pools(event, capacities_to_add, [AbakusGroup.objects.get(name='Abakus')])
         self.assertEqual(sum(capacities_to_add), event.capacity)
 
 
