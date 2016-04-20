3b66cbc00a2b126cee81a05d035343043b04c37a
Orhan Hirsch <orhanhenrik@gmail.com>
2016-04-20 20:58:49 +0200

    Fixed event serializer for comments to work

--- lego/app/events/serializers.py
+++ lego/app/events/serializers.py
@@ -1,8 +1,15 @@
 from basis.serializers import BasisSerializer
+from rest_framework.fields import CharField
 
+from lego.app.comments.serializers import CommentSerializer
 from lego.app.events.models import Event
 
 
 class EventSerializer(BasisSerializer):
+    comments = CommentSerializer(read_only=True, many=True)
+    comment_target = CharField(read_only=True)
+
     class Meta:
         model = Event
+        fields = ('id', 'title', 'ingress', 'text', 'comments', 'comment_target', 'end_time',
+                  'event_type', 'location', 'author', 'start_time')
