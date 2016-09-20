750c8af1611601cb3ca347101a1c3e999df78bff
Marius Kotlarz <marius.kotlarz@gmail.com>
2016-09-20 19:02:36 +0200

    Seperate the slug from the content model

--- lego/apps/articles/models.py
+++ lego/apps/articles/models.py
@@ -1,10 +1,10 @@
 from basis.models import BasisModel
 
-from lego.apps.content.models import Content
+from lego.apps.content.models import SlugContent
 from lego.apps.permissions.models import ObjectPermissionsModel
 
 
-class Article(Content, BasisModel, ObjectPermissionsModel):
+class Article(SlugContent, BasisModel, ObjectPermissionsModel):
     class Meta:
         # This is needed since all the models we inherit from
         # are abstract:
