ddc803361cd05aaa24c90478f4fc41e8a8ed11ed
Martin Thoresen <martinhath@gmail.com>
2016-09-27 21:25:21 +0200

    Add `setUp` in meetings/test_models

--- lego/apps/meetings/tests/test_models.py
+++ lego/apps/meetings/tests/test_models.py
@@ -8,58 +8,62 @@ class MeetingTestCase(TestCase):
     fixtures = ['initial_abakus_groups.yaml', 'development_meetings.yaml',
                 'test_users.yaml']
 
+    def setUp(self):
+        self.user = User.objects.get(id=1)
+        self.meeting = Meeting.objects.get(id=1)
+
     def test_can_invite(self):
-        user = User.objects.get(id=1)
-        meeting = Meeting.objects.get(id=1)
-        invitation = meeting.invite(user)[0]
-        self.assertEqual(invitation.user, user)
-        self.assertEqual(invitation.meeting, meeting)
+        invitation = self.meeting.invite(self.user)[0]
+        self.assertEqual(invitation.user, self.user)
+        self.assertEqual(invitation.meeting, self.meeting)
 
     def test_can_accept_invite(self):
         """
         Check that a user may be invited, can accept the invitation,
-        and is in the list of participants for the meeting.
+        and is in the list of participants.all() for the meeting.
         """
-        user = User.objects.get(id=1)
-        meeting = Meeting.objects.get(id=1)
-        invitation = meeting.invite(user)[0]
+        invitation = self.meeting.invite(self.user)[0]
         invitation.accept()
-        self.assertIn(user, meeting.participants())
+        self.assertIn(self.user, self.meeting.participants.all())
 
     def test_double_invite(self):
-        user = User.objects.get(id=1)
-        meeting = Meeting.objects.get(id=1)
-        self.assertEqual(len(meeting.invited_users.all()), 0)
-        self.assertEqual(len(meeting.participants()), 0)
-        self.assertEqual(len(meeting.invitation.all()), 0)
-        meeting.invite(user)[0].accept()
-        meeting.invite(user)[0].accept()
-        self.assertEqual(len(meeting.invited_users.all()), 1)
-        self.assertEqual(len(meeting.participants()), 1)
-        self.assertEqual(len(meeting.invitation.all()), 1)
+        self.assertEqual(len(self.meeting.invited_users.all()), 0)
+        self.assertEqual(len(self.meeting.participants.all()), 0)
+        self.assertEqual(len(self.meeting.invitation.all()), 0)
+        self.meeting.invite(self.user)[0].accept()
+        self.meeting.invite(self.user)[0].accept()
+        self.assertEqual(len(self.meeting.invited_users.all()), 1)
+        self.assertEqual(len(self.meeting.participants.all()), 1)
+        self.assertEqual(len(self.meeting.invitation.all()), 1)
 
     def test_participants(self):
-        user = User.objects.get(id=1)
-        meeting = Meeting.objects.get(id=1)
-        self.assertEqual(len(meeting.participants()), 0)
-        meeting.invite(user)
-        self.assertEqual(len(meeting.participants()), 0)
-        invitation = meeting.invitation.get(user=user)
+        self.assertEqual(len(self.meeting.participants.all()), 0)
+        self.meeting.invite(self.user)
+        self.assertEqual(len(self.meeting.participants.all()), 0)
+        invitation = self.meeting.invitation.get(user=self.user)
         invitation.accept()
-        self.assertEqual(len(meeting.participants()), 1)
+        self.assertEqual(len(self.meeting.participants.all()), 1)
         invitation.reject()
-        self.assertEqual(len(meeting.participants()), 0)
+        self.assertEqual(len(self.meeting.participants.all()), 0)
 
     def test_invited_users(self):
-        user = User.objects.get(id=1)
-        meeting = Meeting.objects.get(id=1)
-        self.assertEqual(len(meeting.invited_users.all()), 0)
-        meeting.invite(user)
-        self.assertEqual(len(meeting.invited_users.all()), 1)
-        invitation = meeting.invitation.get(user=user)
+        self.assertEqual(len(self.meeting.invited_users.all()), 0)
+        self.meeting.invite(self.user)
+        self.assertEqual(len(self.meeting.invited_users.all()), 1)
+        invitation = self.meeting.invitation.get(user=self.user)
         invitation.accept()
-        self.assertEqual(len(meeting.invited_users.all()), 1)
+        self.assertEqual(len(self.meeting.invited_users.all()), 1)
         invitation.reject()
-        self.assertEqual(len(meeting.invited_users.all()), 1)
-        meeting.uninvite(user)
-        self.assertEqual(len(meeting.invited_users.all()), 0)
+        self.assertEqual(len(self.meeting.invited_users.all()), 1)
+        self.meeting.uninvite(self.user)
+        self.assertEqual(len(self.meeting.invited_users.all()), 0)
+
+    def test_delete_invitation_after_accept(self):
+        self.meeting.invite(self.user)
+        invitation = self.meeting.invitation.get(user=self.user)
+        invitation.accept()
+        self.meeting.uninvite(self.user)
+        self.assertEqual(len(self.meeting.invited_users.all()), 0)
+        self.assertEqual(len(self.meeting.participants.all()), 0)
+        self.assertEqual(len(self.meeting.invitation.all()), 0)
+
