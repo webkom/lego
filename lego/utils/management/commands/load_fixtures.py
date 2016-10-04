49bb19fc5008d4d7290ad7f36610f9c959699957
Martin Thoresen <martinhath@gmail.com>
2016-10-04 20:34:14 +0200

    Replace `load_from_fixture` with `django.core.management.call_command`

--- lego/utils/management/commands/load_fixtures.py
+++ lego/utils/management/commands/load_fixtures.py
@@ -1,47 +1,22 @@
-import os
-
 from django.conf import settings
-from django.core import serializers
-from django.core.management import BaseCommand
-
-from lego.apps.articles.models import Article
-from lego.apps.events.models import Event, Pool, Registration
-from lego.apps.quotes.models import Quote
-from lego.apps.users.models import AbakusGroup, Membership, User
-
-
-def create_if_missing(obj, model):
-    if not model.objects.filter(pk=obj.object.pk).exists():
-        obj.save()
+from django.core.management import BaseCommand, call_command
 
 
 class Command(BaseCommand):
     help = 'Loads initial data from fixtures.'
 
-    def load_from_fixture(self, fixture_path, model):
-        self.stdout.write('Loading fixture %s' % fixture_path)
-
-        fixture_file = os.path.join(settings.BASE_DIR, fixture_path)
-
-        with open(fixture_file) as fixture:
-            for obj in serializers.deserialize('yaml', fixture):
-                create_if_missing(obj, model)
-
     def handle(self, *args, **options):
         self.stdout.write('Loading regular fixtures:')
-        self.load_from_fixture('apps/users/fixtures/initial_abakus_groups.yaml', AbakusGroup)
-        self.load_from_fixture('apps/users/fixtures/initial_users.yaml', User)
-        self.load_from_fixture('apps/users/fixtures/initial_memberships.yaml', Membership)
+        call_command('loaddata', 'lego/apps/users/fixtures/initial_abakus_groups.yaml')
+        call_command('loaddata', 'lego/apps/users/fixtures/initial_users.yaml')
+        call_command('loaddata', 'lego/apps/users/fixtures/initial_memberships.yaml')
 
         if settings.DEVELOPMENT:
             self.stdout.write('Loading development fixtures:')
-            self.load_from_fixture('apps/users/fixtures/development_users.yaml', User)
-            self.load_from_fixture('apps/events/fixtures/development_events.yaml', Event)
-            self.load_from_fixture('apps/events/fixtures/development_pools.yaml', Pool)
-            self.load_from_fixture(
-                'apps/events/fixtures/development_registrations.yaml',
-                Registration
-            )
-            self.load_from_fixture('apps/articles/fixtures/development_articles.yaml', Article)
-            self.load_from_fixture('apps/quotes/fixtures/development_quotes.yaml', Quote)
+            call_command('loaddata', 'lego/apps/users/fixtures/development_users.yaml')
+            call_command('loaddata', 'lego/apps/events/fixtures/development_events.yaml')
+            call_command('loaddata', 'lego/apps/events/fixtures/development_pools.yaml')
+            call_command('loaddata', 'lego/apps/events/fixtures/development_registrations.yaml')
+            call_command('loaddata', 'lego/apps/articles/fixtures/development_articles.yaml')
+            call_command('loaddata', 'lego/apps/quotes/fixtures/development_quotes.yaml')
         self.stdout.write('Done!')
