f4434923cb0dbbab04d72c9f297de288f2d2936b
Martin Thoresen <martinhath@gmail.com>
2016-10-04 20:52:36 +0200

    Move `initial_*` files to the end
    as `loaddata` overrides data, development users will replace initial
    users in the db after loading everything.

--- lego/utils/management/commands/load_fixtures.py
+++ lego/utils/management/commands/load_fixtures.py
@@ -7,10 +7,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('Loading regular fixtures:')
-        call_command('loaddata', 'lego/apps/users/fixtures/initial_abakus_groups.yaml')
-        call_command('loaddata', 'lego/apps/users/fixtures/initial_users.yaml')
-        call_command('loaddata', 'lego/apps/users/fixtures/initial_memberships.yaml')
-
         if settings.DEVELOPMENT:
             self.stdout.write('Loading development fixtures:')
             call_command('loaddata', 'lego/apps/users/fixtures/development_users.yaml')
@@ -19,4 +15,7 @@ class Command(BaseCommand):
             call_command('loaddata', 'lego/apps/events/fixtures/development_registrations.yaml')
             call_command('loaddata', 'lego/apps/articles/fixtures/development_articles.yaml')
             call_command('loaddata', 'lego/apps/quotes/fixtures/development_quotes.yaml')
+        call_command('loaddata', 'lego/apps/users/fixtures/initial_abakus_groups.yaml')
+        call_command('loaddata', 'lego/apps/users/fixtures/initial_users.yaml')
+        call_command('loaddata', 'lego/apps/users/fixtures/initial_memberships.yaml')
         self.stdout.write('Done!')
