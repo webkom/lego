0a54f1cece8f3d20a99b86441bbd5587301a91ea
Edvard Bakken <edvard.vb@live.no>
2016-10-18 19:28:03 +0200

    Refactor model tests to fit earlier changes

--- lego/apps/events/tests/test_models.py
+++ lego/apps/events/tests/test_models.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from django.test import TestCase
 from django.utils import timezone
 
-from lego.apps.events.exceptions import NoAvailablePools, UserNotAdmin
+from lego.apps.events.exceptions import NoAvailablePools, NoSuchPool
 from lego.apps.events.models import Event, Pool, Registration
 from lego.apps.users.models import AbakusGroup, User
 
@@ -704,21 +704,19 @@ class AdminRegistrationTestCase(TestCase):
 
     def test_admin_registration(self):
         event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
-        request_user, user = get_dummy_users(2)
-        event.created_by = request_user
+        user = get_dummy_users(1)[0]
         pool = event.pools.first()
 
         no_of_regs_before = event.number_of_registrations
         pool_no_of_regs_before = pool.registrations.count()
 
-        event.admin_register(request_user, user, pool)
+        event.admin_register(user, pool)
         self.assertEqual(event.number_of_registrations, no_of_regs_before + 1)
         self.assertEqual(pool.registrations.count(), pool_no_of_regs_before + 1)
 
     def test_ar_with_wrong_pool(self):
         event_one = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
-        request_user, user = get_dummy_users(2)
-        event_one.created_by = request_user
+        user = get_dummy_users(1)[0]
         event_two = Event.objects.get(title='POOLS_WITH_REGISTRATIONS')
         wrong_pool = event_two.pools.first()
 
@@ -726,90 +724,81 @@ class AdminRegistrationTestCase(TestCase):
         e2_no_of_regs_before = event_two.number_of_registrations
         pool_no_of_regs_before = wrong_pool.registrations.count()
 
-        with self.assertRaises(ValueError):
-            event_one.admin_register(request_user, user, wrong_pool)
+        with self.assertRaises(NoSuchPool):
+            event_one.admin_register(user, wrong_pool)
         self.assertEqual(event_one.number_of_registrations, e1_no_of_regs_before)
         self.assertEqual(event_two.number_of_registrations, e2_no_of_regs_before)
         self.assertEqual(wrong_pool.registrations.count(), pool_no_of_regs_before)
 
-    def test_ar_without_permissions_for_admin(self):
-        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
-        request_user, user = get_dummy_users(2)
-        pool = event.pools.first()
-
-        e1_no_of_regs_before = event.number_of_registrations
-        pool_no_of_regs_before = pool.registrations.count()
-
-        with self.assertRaises(UserNotAdmin):
-            event.admin_register(request_user, user, pool)
-        self.assertEqual(event.number_of_registrations, e1_no_of_regs_before)
-        self.assertEqual(pool.registrations.count(), pool_no_of_regs_before)
-
     def test_ar_without_permissions_for_user(self):
         event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
-        request_user, user = get_dummy_users(2)
-        event.created_by = request_user
+        user = get_dummy_users(1)[0]
         pool = event.pools.get(name='Webkom')
         AbakusGroup.objects.get(name='Abakus').add_user(user)
 
         e1_no_of_regs_before = event.number_of_registrations
         pool_no_of_regs_before = pool.registrations.count()
 
-        event.admin_register(request_user, user, pool)
+        event.admin_register(user, pool)
         self.assertEqual(event.number_of_registrations, e1_no_of_regs_before+1)
         self.assertEqual(pool.registrations.count(), pool_no_of_regs_before+1)
 
     def test_ar_after_merge(self):
         event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
         event.merge_time = timezone.now() - timedelta(hours=12)
-        request_user, user = get_dummy_users(2)
-        event.created_by = request_user
+        user = get_dummy_users(1)[0]
         pool = event.pools.first()
 
         e1_no_of_regs_before = event.number_of_registrations
         pool_no_of_regs_before = pool.registrations.count()
 
-        event.admin_register(request_user, user, pool)
+        event.admin_register(user, pool)
         self.assertEqual(event.number_of_registrations, e1_no_of_regs_before+1)
         self.assertEqual(pool.registrations.count(), pool_no_of_regs_before+1)
 
     def test_ar_to_full_pool(self):
         event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
         users = get_dummy_users(5)
-        request_user = users[3]
         user = users[4]
-        for u in users[:3]:
+        for u in users[:4]:
             AbakusGroup.objects.get(name='Abakus').add_user(u)
             event.register(u)
-
-        event.created_by = request_user
         pool = event.pools.first()
 
         e1_no_of_regs_before = event.number_of_registrations
         pool_no_of_regs_before = pool.registrations.count()
 
-        event.admin_register(request_user, user, pool)
+        event.admin_register(user, pool)
         self.assertEqual(event.number_of_registrations, e1_no_of_regs_before+1)
         self.assertEqual(pool.registrations.count(), pool_no_of_regs_before+1)
 
     def test_ar_to_full_event(self):
         event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
         users = get_dummy_users(7)
-        request_user = users[5]
         user = users[6]
-        for u in users[:5]:
+        for u in users[:6]:
             AbakusGroup.objects.get(name='Webkom').add_user(u)
             event.register(u)
-
-        event.created_by = request_user
         pool = event.pools.first()
 
         e1_no_of_regs_before = event.number_of_registrations
         pool_no_of_regs_before = pool.registrations.count()
 
-        event.admin_register(request_user, user, pool)
+        event.admin_register(user, pool)
         self.assertEqual(event.number_of_registrations, e1_no_of_regs_before+1)
         self.assertEqual(pool.registrations.count(), pool_no_of_regs_before+1)
+
+    def test_ar_twice(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        user = get_dummy_users(1)[0]
+        pool = event.pools.get(name='Webkom')
+        AbakusGroup.objects.get(name='Abakus').add_user(user)
+
+        e1_no_of_regs_before = event.number_of_registrations
+
+        event.admin_register(user, pool)
+        event.admin_register(user, pool)
+        self.assertEqual(event.number_of_registrations, e1_no_of_regs_before+1)
 
 
 class AssertInvariant:
--- lego/apps/events/views.py
+++ lego/apps/events/views.py
@@ -1,4 +1,4 @@
-from rest_framework import filters, viewsets, decorators, status
+from rest_framework import decorators, filters, status, viewsets
 from rest_framework.response import Response
 
 from lego.apps.events.filters import EventsFilterSet
