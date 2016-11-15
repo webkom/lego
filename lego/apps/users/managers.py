1a8f77de759e07dca6e5b6cebf714e1be7da7d8b
Edvard Bakken <edvard.vb@live.no>
2016-11-15 18:11:24 +0100

    Remove permissions class. Move helper-methods from penalty manager to penalty model.

--- lego/apps/users/managers.py
+++ lego/apps/users/managers.py
@@ -1,8 +1,5 @@
-from datetime import timedelta
-
 from django.utils import timezone
 
-from lego.settings.lego import PENALTY_DURATION, PENALTY_IGNORE_SUMMER, PENALTY_IGNORE_WINTER
 from lego.utils.managers import PersistentModelManager
 
 
@@ -25,35 +22,7 @@ class MembershipManager(PersistentModelManager):
 class UserPenaltyManager(PersistentModelManager):
 
     def valid(self):
+        from lego.apps.users.models import Penalty
+        offset = Penalty.penalty_offset(timezone.now(), False)
         return super(UserPenaltyManager, self).get_queryset().\
-            filter(created_at__gt=timezone.now() - self.penalty_offset(timezone.now(), False))
-
-    def penalty_offset(self, start_date, forwards=True):
-
-        remaining_days = PENALTY_DURATION.days
-        offset_days = 0
-        multiplier = 1 if forwards else -1
-
-        while remaining_days > 0:
-
-            date_to_check = start_date + (multiplier * timedelta(days=offset_days))
-
-            if not self.ignore_date(date_to_check):
-                remaining_days -= 1
-
-            offset_days += 1
-
-        return timedelta(days=offset_days)
-
-    def ignore_date(self, date):
-        summer_from, summer_to = PENALTY_IGNORE_SUMMER
-        winter_from, winter_to = PENALTY_IGNORE_WINTER
-        if summer_from \
-                < (date.month, date.day) \
-                < summer_to:
-            return True
-        elif winter_to \
-                < (date.month, date.day) \
-                < winter_from:
-            return False
-        return True
+            filter(created_at__gt=timezone.now() - offset)
