057b105973d7639aebd9990994e856038b17ff3c
Hans-Wilhelm Warlo <hw@warlo.no>
2016-12-08 13:23:52 +0100

    Add camelize on websocket data

--- lego/apps/events/websockets.py
+++ lego/apps/events/websockets.py
@@ -1,17 +1,13 @@
 from datetime import datetime
 
-from channels import Group
+from djangorestframework_camel_case.render import camelize
 
 from lego.apps.events.serializers import EventReadDetailedSerializer, RegistrationReadSerializer
 from lego.apps.permissions.filters import filter_queryset
-from lego.apps.websockets.handlers import group_for_user
+from lego.apps.websockets.handlers import group_for_event
 from lego.apps.websockets.notifiers import notify_group
 
 from .models import Event
-
-
-def get_group_for_event(event):
-    return Group(f'event-{event.pk}')
 
 
 def find_event_groups(user):
@@ -23,33 +19,34 @@ def find_event_groups(user):
     queryset = filter_queryset(user, after_now)
     groups = []
     for event in queryset.all():
-        groups.append(get_group_for_event(event))
+        groups.append(group_for_event(event))
 
     return groups
 
 
 def notify_registration(type, registration):
-    group = group_for_user(registration.user)
+    group = group_for_event(registration.event)
     serializer = RegistrationReadSerializer(registration)
     notify_group(group, {
         'type': type,
-        'payload': serializer.data
+        'payload': camelize(serializer.data)
     })
 
 
 def notify_unregistration(type, registration, pool_id):
-    group = group_for_user(registration.user)
+    group = group_for_event(registration.event)
     serializer = RegistrationReadSerializer(registration)
     payload = serializer.data
     payload['from_pool'] = pool_id
+
     notify_group(group, {
         'type': type,
-        'payload': payload
+        'payload': camelize(payload)
     })
 
 
 def event_updated_notifier(event):
-    group = get_group_for_event(event)
+    group = group_for_event(event)
     serializer = EventReadDetailedSerializer(event)
     notify_group(group, {
         'type': 'EVENT_UPDATED',
