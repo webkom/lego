07e20decbfa44debec105b5267e23bffe2c4e6fb
Martin Ek <mail@ekmartin.com>
2016-12-30 00:22:42 +0100

    flatpages: Remove hierarchy API in favor of list

--- lego/apps/flatpages/views.py
+++ lego/apps/flatpages/views.py
@@ -1,22 +1,19 @@
 from rest_framework import viewsets
-from rest_framework.decorators import detail_route
-from rest_framework.response import Response
 
 from lego.apps.permissions.views import PermissionsMixin
 
 from .models import Page
-from .serializers import PageSerializer
+from .serializers import PageDetailSerializer, PageListSerializer
 
 
 class PageViewSet(PermissionsMixin, viewsets.ModelViewSet):
     queryset = Page.objects.all()
-    serializer_class = PageSerializer
     order_by = 'created_at'
     lookup_field = 'slug'
+    pagination_class = None
 
-    @detail_route(methods=['GET'])
-    def hierarchy(self, request, slug=None):
-        page = self.queryset.get(slug=slug)
-        tree = page.get_family()
-        serialized = PageSerializer(tree, many=True)
-        return Response(serialized.data)
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PageListSerializer
+
+        return PageDetailSerializer
