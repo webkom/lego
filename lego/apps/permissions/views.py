6d3d78452432da2b00f97783439a838eb367eccf
Martin Ek <mail@ekmartin.com>
2016-12-30 01:12:19 +0100

    Make sure PermissionsMixin works without pagination

--- lego/apps/permissions/views.py
+++ lego/apps/permissions/views.py
@@ -1,6 +1,19 @@
 from .utils import get_viewset_permissions
 
 
+def wrap_results(response):
+    """
+    Turns response.data into a dict,
+    in case the inheritee isn't using pagination.
+    """
+    if isinstance(response.data, list):
+        return {
+            'results': response.data
+        }
+
+    return response.data
+
+
 class PermissionsMixin:
     """
     Append a `permission` value on list and retrieve methods. This makes it possible for a
@@ -8,6 +21,7 @@ class PermissionsMixin:
     """
     def list(self, request, *args, **kwargs):
         response = super().list(request, args, kwargs)
+        response.data = wrap_results(response)
         response.data['permissions'] = get_viewset_permissions(
             self, self.get_queryset().model, None, request.user
         )
@@ -15,6 +29,7 @@ class PermissionsMixin:
 
     def retrieve(self, request, *args, **kwargs):
         response = super().retrieve(request, args, kwargs)
+        response.data = wrap_results(response)
         response.data['permissions'] = get_viewset_permissions(
             self, self.get_queryset().model, self.get_object(), request.user
         )
