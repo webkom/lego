4edacc96a480870152ecb36df329541132d2fa08
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2016-12-31 17:14:03 +0100

    Log more file operations and attach the user to the file model

--- lego/apps/files/utils.py
+++ lego/apps/files/utils.py
@@ -5,13 +5,13 @@ from .models import File
 from .storage import storage
 
 
-def prepare_file_upload(key):
+def prepare_file_upload(key, user):
     """
     Create instructions for the client on how to upload a new file. This functions returns two
     values, the first one is the url the file should be uploaded to and the secound value is a
     dict with additional fields the client need to attach to the upload request.
     """
-    file = File.create_file(key)
+    file = File.create_file(key, user)
     redirect_url = '{0}{1}'.format(
         settings.SERVER_URL,
         resolve_url('api:v1:file-upload-success', file.key),
