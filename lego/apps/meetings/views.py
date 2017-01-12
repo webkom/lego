a2a6bd879905262f93fabcafff83868f699ef3cd
Odin Ugedal <odin@ugedal.com>
2017-01-12 20:14:48 +0100

    Add support for bulk invite on meetings

--- lego/apps/meetings/views.py
+++ lego/apps/meetings/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 
 from lego.apps.meetings.models import Meeting, MeetingInvitation
 from lego.apps.meetings.permissions import MeetingInvitationPermissions, MeetingPermissions
-from lego.apps.meetings.serializers import (MeetingGroupInvite, MeetingInvitationSerializer,
+from lego.apps.meetings.serializers import (MeetingBulkInvite, MeetingGroupInvite,
+                                            MeetingInvitationSerializer,
                                             MeetingInvitationUpdateSerializer, MeetingSerializer,
                                             MeetingUserInvite)
 from lego.apps.permissions.views import AllowedPermissionsMixin
@@ -21,7 +22,23 @@ class MeetingViewSet(AllowedPermissionsMixin, viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         meeting.invite_user(user)
-        return Response(data='Invited user ' + str(user.id), status=status.HTTP_201_CREATED)
+        return Response(data='Invited user ' + str(user.id), status=status.HTTP_200_OK)
+
+    @decorators.detail_route(methods=['POST'], serializer_class=MeetingBulkInvite)
+    def bulk_invite(self, request, *args, **kwargs):
+        meeting = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        users = serializer.validated_data['users']
+        groups = serializer.validated_data['groups']
+        if not len(users) and not len(groups):
+            return Response(data='No users or groups given', status=status.HTTP_400_BAD_REQUEST)
+        for user in users:
+            meeting.invite_user(user)
+        for group in groups:
+            meeting.invite_group(group)
+        return Response(data='Invited users {}, and groups {}'.format(users, groups),
+                        status=status.HTTP_200_OK)
 
     @decorators.detail_route(methods=['POST'], serializer_class=MeetingGroupInvite)
     def invite_group(self, request, *args, **kwargs):
@@ -30,7 +47,7 @@ class MeetingViewSet(AllowedPermissionsMixin, viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         group = serializer.validated_data['group']
         meeting.invite_group(group)
-        return Response(data='Invited group ' + str(group.id), status=status.HTTP_201_CREATED)
+        return Response(data='Invited group ' + str(group.id), status=status.HTTP_200_OK)
 
 
 class MeetingInvitationViewSet(AllowedPermissionsMixin, viewsets.ModelViewSet):
