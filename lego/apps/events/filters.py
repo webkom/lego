8fd073ed71a7aa5caaa7ccc084f8dc03637f6120
Hans-Wilhelm Warlo <hw@warlo.no>
2017-01-18 13:34:42 +0100

    Add datestamp filtering instead of queryset filtering

--- lego/apps/events/filters.py
+++ lego/apps/events/filters.py
@@ -1,4 +1,4 @@
-from django_filters import CharFilter, FilterSet
+from django_filters import CharFilter, DateFilter, FilterSet
 
 from lego.apps.events.models import Event
 
@@ -6,7 +6,9 @@ from lego.apps.events.models import Event
 class EventsFilterSet(FilterSet):
     year = CharFilter('start_time__year')
     month = CharFilter('start_time__month')
+    date_after = DateFilter('start_time', lookup_expr='gte')
+    date_before = DateFilter('start_time', lookup_expr='lte')
 
     class Meta:
         model = Event
-        fields = ('year', 'month',)
+        fields = ('year', 'month', 'date_after', 'date_before')
