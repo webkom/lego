53809d73ea766e98817aeca5caa318229a297ef3
Kristoffer K Larsen <kristoffer@larsen.so>
2017-01-26 12:45:43 +0100

    Added cover on article model

--- lego/apps/articles/models.py
+++ lego/apps/articles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from lego.apps.content.models import Content
+from lego.apps.files.models import FileField
 from lego.apps.permissions.models import ObjectPermissionsModel
 from lego.utils.models import BasisModel
 
@@ -8,6 +9,7 @@ from lego.utils.models import BasisModel
 class Article(Content, BasisModel, ObjectPermissionsModel):
 
     author = models.ForeignKey('users.User')
+    cover = FileField()
 
     class Meta:
         abstract = False
