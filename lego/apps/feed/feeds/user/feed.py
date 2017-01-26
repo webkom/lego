268c29c7644e41cb44cb65bf63296fe7a3726f7b
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-01-27 00:26:48 +0100

    Add a sample user manager

--- lego/apps/feed/feeds/user/feed.py
+++ lego/apps/feed/feeds/user/feed.py
@@ -2,8 +2,10 @@ from lego.apps.feed.feed import AggregatedFeed
 
 
 class UserFeed(AggregatedFeed):
-    """
-    This feeds should contains activities related to a user based on friends, and followed events.
-    """
 
     timeline_cf_name = 'user'
+
+
+class PersonalFeed(AggregatedFeed):
+
+    timeline_cf_name = 'personal'
