416269d22344db07071d585374e58d6f54ac84b8
Hans-Wilhelm Warlo <hw@warlo.no>
2017-01-29 21:39:46 +0100

    Refactor event tests to multiple files

---
+++ lego/apps/events/tests/test_event_methods.py
@@ -0,0 +1,150 @@
+from datetime import timedelta
+
+from django.test import TestCase
+from django.utils import timezone
+
+from lego.apps.events.models import Event, Registration
+from lego.apps.users.models import AbakusGroup
+from .utils import get_dummy_users
+
+
+class EventMethodTest(TestCase):
+    fixtures = ['initial_abakus_groups.yaml', 'test_users.yaml', 'test_events.yaml']
+
+    def setUp(self):
+        Event.objects.all().update(start_time=timezone.now() + timedelta(hours=3))
+
+    def test_str(self):
+        event = Event.objects.get(pk=1)
+        self.assertEqual(str(event), event.title)
+
+    def test_calculate_full_pools(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        abakus_pool = event.pools.get(name='Abakusmember')
+        webkom_pool = event.pools.get(name='Webkom')
+        users = get_dummy_users(5)
+        for user in users:
+            AbakusGroup.objects.get(name='Webkom').add_user(user)
+
+        full_pools, open_pools = event.calculate_full_pools([abakus_pool, webkom_pool])
+        self.assertEqual(len(full_pools), 0)
+        self.assertEqual(len(open_pools), 2)
+
+        for user in users[:3]:
+            registration = Registration.objects.get_or_create(event=event, user=user)[0]
+            event.register(registration)
+        full_pools, open_pools = event.calculate_full_pools([abakus_pool, webkom_pool])
+        self.assertEqual(len(full_pools), 1)
+        self.assertEqual(len(open_pools), 1)
+
+        for user in users[3:]:
+            registration = Registration.objects.get_or_create(event=event, user=user)[0]
+            event.register(registration)
+        full_pools, open_pools = event.calculate_full_pools([abakus_pool, webkom_pool])
+        self.assertEqual(len(full_pools), 2)
+        self.assertEqual(len(open_pools), 0)
+
+    def test_doesnt_have_pool_permission(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        user = get_dummy_users(1)[0]
+        abakus_pool = event.pools.get(name='Abakusmember')
+        webkom_pool = event.pools.get(name='Webkom')
+        self.assertFalse(event.has_pool_permission(user, abakus_pool))
+        self.assertFalse(event.has_pool_permission(user, webkom_pool))
+
+    def test_has_some_pool_permissions(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        user = get_dummy_users(1)[0]
+        abakus_pool = event.pools.get(name='Abakusmember')
+        webkom_pool = event.pools.get(name='Webkom')
+        AbakusGroup.objects.get(name='Abakus').add_user(user)
+        self.assertTrue(event.has_pool_permission(user, abakus_pool))
+        self.assertFalse(event.has_pool_permission(user, webkom_pool))
+
+    def test_has_all_pool_permissions(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        user = get_dummy_users(1)[0]
+        abakus_pool = event.pools.get(name='Abakusmember')
+        webkom_pool = event.pools.get(name='Webkom')
+        AbakusGroup.objects.get(name='Webkom').add_user(user)
+        self.assertTrue(event.has_pool_permission(user, abakus_pool))
+        self.assertTrue(event.has_pool_permission(user, webkom_pool))
+
+    def test_find_most_exclusive_pool(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        webkom_pool = event.pools.get(name='Webkom')
+        abakus_pool = event.pools.get(name='Abakusmember')
+
+        users = get_dummy_users(3)
+        user_three = users.pop()
+        for user in users:
+            AbakusGroup.objects.get(name='Abakus').add_user(user)
+        AbakusGroup.objects.get(name='Webkom').add_user(user_three)
+
+        self.assertEqual(event.find_most_exclusive_pools(
+            [webkom_pool, abakus_pool])[0], webkom_pool)
+        self.assertEqual(len(event.find_most_exclusive_pools([webkom_pool, abakus_pool])), 1)
+
+    def test_find_most_exclusive_when_equal(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        webkom_pool = event.pools.get(name='Webkom')
+        abakus_pool = event.pools.get(name='Abakusmember')
+        users = get_dummy_users(3)
+        for user in users:
+            AbakusGroup.objects.get(name='Webkom').add_user(user)
+        self.assertEqual(len(event.find_most_exclusive_pools([webkom_pool, abakus_pool])), 2)
+
+    def test_select_highest_capacity(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        webkom_pool = event.pools.get(name='Webkom')
+        abakus_pool = event.pools.get(name='Abakusmember')
+        self.assertEqual(event.select_highest_capacity([abakus_pool, webkom_pool]), abakus_pool)
+
+    def test_get_earliest_registration_time_without_pools_provided(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        webkom_pool = event.pools.get(name='Webkom')
+        abakus_pool = event.pools.get(name='Abakusmember')
+
+        current_time = timezone.now()
+        webkom_pool.activation_date = current_time
+        webkom_pool.save()
+        abakus_pool.activation_date = current_time - timedelta(hours=1)
+        abakus_pool.save()
+
+        user = get_dummy_users(1)[0]
+        AbakusGroup.objects.get(name='Webkom').add_user(user)
+        earliest_reg = event.get_earliest_registration_time(user)
+
+        self.assertEqual(earliest_reg, abakus_pool.activation_date)
+
+    def test_get_earliest_registration_time_no_penalties(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        webkom_pool = event.pools.get(name='Webkom')
+        abakus_pool = event.pools.get(name='Abakusmember')
+
+        current_time = timezone.now()
+        webkom_pool.activation_date = current_time
+        webkom_pool.save()
+        abakus_pool.activation_date = current_time - timedelta(hours=1)
+        abakus_pool.save()
+
+        user = get_dummy_users(1)[0]
+        AbakusGroup.objects.get(name='Webkom').add_user(user)
+
+        earliest_reg = event.get_earliest_registration_time(user, [webkom_pool, abakus_pool])
+        self.assertEqual(earliest_reg, current_time-timedelta(hours=1))
+
+    def test_number_of_waiting_registrations(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        pool = event.pools.get(name='Abakusmember')
+        people_to_place_in_waiting_list = 3
+        users = get_dummy_users(pool.capacity + 3)
+
+        for user in users:
+            AbakusGroup.objects.get(name='Abakus').add_user(user)
+            registration = Registration.objects.get_or_create(event=event, user=user)[0]
+            event.register(registration)
+
+        self.assertEqual(
+            event.waiting_registrations.count(), people_to_place_in_waiting_list
+        )
---
+++ lego/apps/events/tests/test_pools.py
@@ -0,0 +1,52 @@
+from datetime import timedelta
+
+from django.test import TestCase
+from django.utils import timezone
+
+from lego.apps.events.models import Event, Pool
+from lego.apps.users.models import AbakusGroup
+
+
+class PoolMethodTest(TestCase):
+    fixtures = ['initial_abakus_groups.yaml', 'test_users.yaml', 'test_events.yaml']
+
+    def setUp(self):
+        event = Event.objects.get(title='POOLS_WITH_REGISTRATIONS')
+        self.pool = event.pools.first()
+
+    def test_str(self):
+        self.assertEqual(str(self.pool), self.pool.name)
+
+    def test_delete_pool_with_registrations(self):
+        with self.assertRaises(ValueError):
+            self.pool.delete()
+
+    def test_delete_pool_without_registrations(self):
+        event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
+        pool = event.pools.first()
+        number_of_pools = len(event.pools.all())
+        pool.delete()
+        self.assertEqual(len(event.pools.all()), number_of_pools - 1)
+
+
+class PoolCapacityTestCase(TestCase):
+    fixtures = ['initial_abakus_groups.yaml', 'test_users.yaml', 'test_events.yaml']
+
+    def create_pools(self, event, capacities_to_add, permission_groups):
+        for capacity in capacities_to_add:
+            pool = Pool.objects.create(
+                name='Abakus', capacity=capacity, event=event,
+                activation_date=(timezone.now() - timedelta(hours=24)))
+            pool.permission_groups = permission_groups
+
+    def test_capacity_with_single_pool(self):
+        event = Event.objects.get(title='NO_POOLS_ABAKUS')
+        capacities_to_add = [10]
+        self.create_pools(event, capacities_to_add, [AbakusGroup.objects.get(name='Abakus')])
+        self.assertEqual(sum(capacities_to_add), event.active_capacity)
+
+    def test_capacity_with_multiple_pools(self):
+        event = Event.objects.get(title='NO_POOLS_ABAKUS')
+        capacities_to_add = [10, 20]
+        self.create_pools(event, capacities_to_add, [AbakusGroup.objects.get(name='Abakus')])
+        self.assertEqual(sum(capacities_to_add), event.active_capacity)
