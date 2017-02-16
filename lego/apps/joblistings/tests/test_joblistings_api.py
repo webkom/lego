b9267325e6de29c49c73fb2d8f74520603ab1fd1
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2017-02-16 22:48:48 +0100

    Initial joblistings_create.

--- lego/apps/joblistings/tests/test_joblistings_api.py
+++ lego/apps/joblistings/tests/test_joblistings_api.py
@@ -7,6 +7,52 @@ from rest_framework.test import APITestCase
 from lego.apps.joblistings.models import Joblisting
 from lego.apps.users.models import AbakusGroup, User
 
+_test_joblistings_data = [
+    {
+        'title': 'BEKK - sommerjobb 2017',
+        'description': 'En bedrift.',
+        'text': 'BEKK er et solid programvarehus i kraftig vekst. \
+                 Vi er 245 dyktige medarbeidere, fordelt på hovedkontoret i Bodø og \
+                 avdelingskontorer i Tromsø, Trondheim, Bergen og Oslo. Vi ønsker å \
+                 effektivisere helsevesenet ved å lage en fantastisk elektronisk pasientjournal \
+                 utviklet av dyktige utviklere i samarbeid med en erfaren medisinsk stab',
+        'deadline': '2016-11-03T02:00:00+00:00',
+        'visible_from': '2016-09-30T16:15:00+00:00',
+        'visible_to': '2017-09-30T16:15:00+00:00',
+        'job_type': 'summer_job',
+        'from_year': 3,
+        'to_year': 5,
+    }
+]
+
+_test_company_data = [
+    {
+        'id': 1,
+        'name': 'BEKK',
+        'website': 'bekk.no',
+    }
+]
+
+_test_workplaces_data = [
+    {
+        'town': 'Oslo',
+    },
+    {
+        'town': 'Trondheim',
+    },
+    {
+        'town': 'Bergen',
+    }
+]
+
+_test_companycontact_data = [
+    {
+        'name': 'Abraham Finnsen',
+        'mail': 'abraham@finnsen.no',
+    }
+]
+
+
 
 def _get_list_url():
     return reverse('api:v1:joblisting-list')
@@ -63,3 +109,14 @@ class RetrieveJoblistingsTestCase(APITestCase):
         joblisting_response = self.client.get(_get_detail_url(2))
         self.assertEqual(joblisting_response.data['id'], 2)
         self.assertEqual(joblisting_response.status_code, 200)
+
+
+class CreateJoblistingsTestCase(APITestCase):
+    fixtures = ['development_joblistings.yaml', 'test_users.yaml',
+                'development_companies.yaml', 'initial_abakus_groups.yaml']
+
+    def setUp(self):
+        self.abakus_user = User.objects.all().first()
+        self.client.force_authenticate(self.abakus_user)
+
+    def test_joblistings_creation(self):
