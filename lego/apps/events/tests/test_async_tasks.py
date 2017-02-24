8c4dbac5e446a8a3adc4145202561ae49d626a45
Edvard Bakken <edvard.vb@live.no>
2017-02-24 17:12:42 +0100

    Rework tests to avoid mocks and fix feedback

--- lego/apps/events/tests/test_async_tasks.py
+++ lego/apps/events/tests/test_async_tasks.py
@@ -1,5 +1,4 @@
 from datetime import timedelta
-from unittest import mock
 
 from django.test import TestCase
 from django.utils import timezone
@@ -10,7 +9,6 @@ from lego.apps.events.tasks import (async_register, bump_waiting_users_to_new_po
                                     check_events_for_registrations_with_expired_penalties)
 from lego.apps.events.tests.utils import get_dummy_users
 from lego.apps.users.models import AbakusGroup, Penalty
-from lego.utils.test_utils import fake_time
 
 
 class PoolActivationTestCase(APITestCase):
@@ -112,31 +110,22 @@ class PoolActivationTestCase(APITestCase):
         self.assertEqual(self.pool_two.registrations.count(), 0)
         self.assertEqual(self.event.waiting_registrations.count(), 1)
 
-    number_of_calls = 40
-
-    @mock.patch('django.utils.timezone.now',
-                side_effect=[fake_time(2016, 10, 1) + timedelta(milliseconds=i)
-                             for i in range(number_of_calls)])
-    def test_isnt_bumped_with_penalties(self, mock_now):
+    def test_isnt_bumped_with_penalties(self):
         """ Users should not be bumped if they have 3 penalties. """
-        self.event.start_time = mock_now() + timedelta(days=1)
-        self.event.merge_time = mock_now() + timedelta(hours=12)
+        self.event.start_time = timezone.now() + timedelta(days=1)
+        self.event.merge_time = timezone.now() + timedelta(hours=12)
         self.event.save()
 
-        self.pool_one.activation_date = mock_now() - timedelta(days=1)
+        self.pool_one.activation_date = timezone.now() - timedelta(days=1)
         self.pool_one.save()
 
-        self.pool_two.activation_date = mock_now() + timedelta(minutes=30)
+        self.pool_two.activation_date = timezone.now() + timedelta(minutes=30)
         self.pool_two.save()
 
         users = get_dummy_users(2)
 
         Penalty.objects.create(
-            user=users[1],
-            reason='test',
-            weight=3,
-            source_event=self.event,
-            created_at=mock_now()
+            user=users[1], reason='test', weight=3, source_event=self.event,
         )
 
         for user in users:
@@ -199,199 +188,125 @@ class PenaltyExpiredTestCase(TestCase):
     def setUp(self):
         self.event = Event.objects.get(title='POOLS_NO_REGISTRATIONS')
         self.event.heed_penalties = True
-
+        self.event.start_time = timezone.now() + timedelta(days=1)
+        self.event.merge_time = timezone.now() + timedelta(hours=12)
         self.event.save()
+        for pool in self.event.pools.all():
+            pool.activation_time = timezone.now() - timedelta(days=1)
+            pool.capacity = 1
+            pool.save()
 
     def tearDown(self):
         from django_redis import get_redis_connection
         get_redis_connection("default").flushall()
 
-    def mocked_setup(self, mock_now):
-        self.event.start_time = mock_now() + timedelta(days=1)
-        self.event.merge_time = mock_now() + timedelta(hours=12)
-        self.event.save()
-        for pool in self.event.pools.all():
-            pool.activation_time = mock_now() - timedelta(days=1)
-            pool.capacity = 1
-            pool.save()
-
-    def make_penalty_expire(self, penalty, mock_now):
-        penalty.created_at = mock_now() - timedelta(days=21)
+    def make_penalty_expire(self, penalty):
+        penalty.created_at = timezone.now() - timedelta(days=365)
         penalty.save()
 
-    number_of_calls = 43
-
-    @mock.patch('django.utils.timezone.now',
-                side_effect=[fake_time(2016, 10, 1) + timedelta(milliseconds=i)
-                             for i in range(number_of_calls)])
-    def test_is_automatically_bumped_after_penalty_expiration(self, mock_now):
-        """ Tests that a task for automatic bump is created if user has 3 penalties,
-            and that the user is bumped when they expire"""
-        self.mocked_setup(mock_now)
+    def test_is_automatically_bumped_after_penalty_expiration(self):
+        """ Tests that the user that registered with penalties is bumped
+            by the task after penalty expiration"""
 
         user = get_dummy_users(1)[0]
         AbakusGroup.objects.get(name='Abakus').add_user(user)
 
-        p1 = Penalty.objects.create(
-            user=user,
-            reason='test',
-            weight=3,
-            source_event=self.event,
-            created_at=mock_now()
-        )
+        p1 = Penalty.objects.create(user=user, reason='test', weight=3, source_event=self.event)
 
         registration = Registration.objects.get_or_create(event=self.event, user=user)[0]
         async_register(registration.id)
-        self.make_penalty_expire(p1, mock_now)
+
+        self.make_penalty_expire(p1)
         check_events_for_registrations_with_expired_penalties.delay()
+
         self.assertIsNotNone(Registration.objects.get(id=registration.id).pool)
         self.assertEqual(self.event.number_of_registrations, 1)
 
-    number_of_calls = 43
-
-    @mock.patch('django.utils.timezone.now',
-                side_effect=[fake_time(2016, 10, 1) + timedelta(milliseconds=i)
-                             for i in range(number_of_calls)])
-    def test_is_bumped_with_multiple_penalties(self, mock_now):
+    def test_is_bumped_with_multiple_penalties(self):
         """ Tests that a user is bumped when going from 4 to 2 active penalties"""
-        self.mocked_setup(mock_now)
 
         user = get_dummy_users(1)[0]
         AbakusGroup.objects.get(name='Abakus').add_user(user)
 
-        p1 = Penalty.objects.create(
-            user=user,
-            reason='test',
-            weight=2,
-            source_event=self.event,
-            created_at=mock_now()
-        )
-
-        Penalty.objects.create(
-            user=user,
-            reason='test2',
-            weight=2,
-            source_event=self.event,
-            created_at=mock_now()
-        )
+        p1 = Penalty.objects.create(user=user, reason='test', weight=2, source_event=self.event)
+        Penalty.objects.create(user=user, reason='test2', weight=2, source_event=self.event)
 
         registration = Registration.objects.get_or_create(event=self.event, user=user)[0]
         async_register(registration.id)
-        self.make_penalty_expire(p1, mock_now)
+
+        self.make_penalty_expire(p1)
         check_events_for_registrations_with_expired_penalties.delay()
+
         self.assertIsNotNone(Registration.objects.get(id=registration.id).pool)
         self.assertEqual(self.event.number_of_registrations, 1)
 
-    number_of_calls = 29
-
-    @mock.patch('django.utils.timezone.now',
-                side_effect=[fake_time(2016, 10, 1) + timedelta(milliseconds=i)
-                             for i in range(number_of_calls)])
-    def test_isnt_bumped_with_too_many_penalties(self, mock_now):
+    def test_isnt_bumped_with_too_many_penalties(self):
         """ Tests that a user isn't bumped when going from 4 to 3 active penalties """
-        self.mocked_setup(mock_now)
 
         user = get_dummy_users(1)[0]
         AbakusGroup.objects.get(name='Abakus').add_user(user)
 
-        p1 = Penalty.objects.create(
-            user=user,
-            reason='test',
-            weight=1,
-            source_event=self.event,
-            created_at=mock_now()
-        )
-
-        Penalty.objects.create(
-            user=user,
-            reason='test2',
-            weight=3,
-            source_event=self.event,
-            created_at=mock_now()
-        )
+        p1 = Penalty.objects.create(user=user, reason='test', weight=1, source_event=self.event)
+        Penalty.objects.create(user=user, reason='test2', weight=3, source_event=self.event)
 
         registration = Registration.objects.get_or_create(event=self.event, user=user)[0]
         async_register(registration.id)
-        self.make_penalty_expire(p1, mock_now)
+
+        self.make_penalty_expire(p1)
         check_events_for_registrations_with_expired_penalties.delay()
+
         self.assertIsNone(Registration.objects.get(id=registration.id).pool)
         self.assertEqual(self.event.number_of_registrations, 0)
 
-    number_of_calls = 43
-
-    @mock.patch('django.utils.timezone.now',
-                side_effect=[fake_time(2016, 10, 1) + timedelta(milliseconds=i)
-                             for i in range(number_of_calls)])
-    def test_isnt_bumped_when_full(self, mock_now):
+    def test_isnt_bumped_when_full(self):
         """ Tests that a user isnt bumped when the event is full when penalties expire. """
-        self.mocked_setup(mock_now)
 
         users = get_dummy_users(2)
         for user in users:
             AbakusGroup.objects.get(name='Abakus').add_user(user)
 
         p1 = Penalty.objects.create(
-            user=users[1],
-            reason='test',
-            weight=3,
-            source_event=self.event,
-            created_at=mock_now()
+            user=users[1], reason='test', weight=3, source_event=self.event
         )
 
         for user in users:
             registration = Registration.objects.get_or_create(event=self.event, user=user)[0]
             async_register(registration.id)
 
-        self.make_penalty_expire(p1, mock_now)
+        self.make_penalty_expire(p1)
         check_events_for_registrations_with_expired_penalties.delay()
 
         self.assertIsNone(Registration.objects.get(user=users[1]).pool)
         self.assertEqual(self.event.number_of_registrations, 1)
 
-    number_of_calls = 63
-
-    @mock.patch('django.utils.timezone.now',
-                side_effect=[fake_time(2016, 10, 1) + timedelta(milliseconds=i)
-                             for i in range(number_of_calls)])
-    def test_isnt_bumped_when_not_first_in_line(self, mock_now):
+    def test_isnt_bumped_when_not_first_in_line(self):
         """ Tests that a user isnt bumped when not first in the waiting list.
             In practice, this should never happen, because the only reason someone
             is in front of you in the list is if the event is full, which is tested above.
         """
-        self.mocked_setup(mock_now)
 
         users = get_dummy_users(3)
         for user in users:
             AbakusGroup.objects.get(name='Abakus').add_user(user)
 
         p1 = Penalty.objects.create(
-            user=users[2],
-            reason='test',
-            weight=3,
-            source_event=self.event,
-            created_at=mock_now()
+            user=users[2], reason='test', weight=3, source_event=self.event
         )
 
         for user in users:
             registration = Registration.objects.get_or_create(event=self.event, user=user)[0]
             async_register(registration.id)
 
-        self.make_penalty_expire(p1, mock_now)
+        self.make_penalty_expire(p1)
         check_events_for_registrations_with_expired_penalties.delay()
 
         self.assertIsNone(Registration.objects.get(user=users[1]).pool)
         self.assertIsNone(Registration.objects.get(user=users[2]).pool)
         self.assertEqual(self.event.number_of_registrations, 1)
 
-    number_of_calls = 63
-
-    @mock.patch('django.utils.timezone.now',
-                side_effect=[fake_time(2016, 10, 1) + timedelta(milliseconds=i)
-                             for i in range(number_of_calls)])
-    def test_async_bump_post_merge(self, mock_now):
+    def test_async_bump_post_merge(self):
         """ Tests that a waiting user with penalties is bumped to any pool after merge"""
-        self.mocked_setup(mock_now)
-        self.event.merge_time = mock_now()
+        self.event.merge_time = timezone.now()
         self.event.save()
 
         users = get_dummy_users(2)
@@ -399,18 +314,14 @@ class PenaltyExpiredTestCase(TestCase):
             AbakusGroup.objects.get(name='Abakus').add_user(user)
 
         p1 = Penalty.objects.create(
-            user=users[1],
-            reason='test',
-            weight=3,
-            source_event=self.event,
-            created_at=mock_now()
+            user=users[1], reason='test', weight=3, source_event=self.event
         )
 
         for user in users:
             registration = Registration.objects.get_or_create(event=self.event, user=user)[0]
             async_register(registration.id)
 
-        self.make_penalty_expire(p1, mock_now)
+        self.make_penalty_expire(p1)
         check_events_for_registrations_with_expired_penalties.delay()
 
         self.assertIsNotNone(Registration.objects.get(user=users[1]).pool)
