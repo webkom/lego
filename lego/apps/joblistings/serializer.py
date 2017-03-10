93103405c55c5966101c6c6498439c5c77560e55
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2017-03-11 00:43:55 +0100

    Add create() in createAndUpdateSerializer.

--- lego/apps/joblistings/serializer.py
+++ lego/apps/joblistings/serializer.py
@@ -39,3 +39,12 @@ class JoblistingCreateAndUpdateSerializer(BasisModelSerializer):
         fields = ('id', 'title', 'text', 'company', 'responsible', 'description', 'deadline',
                   'visible_from', 'visible_to', 'job_type', 'workplaces', 'from_year',
                   'to_year', 'application_url')
+
+    def create(self, validated_data):
+        workplaces_data = validated_data.pop('workplaces')
+        joblisting = Joblisting.objects.create(**validated_data)
+
+        for workplace in workplaces_data:
+            workplace, created = Workplace.objects.get_or_create(name=workplace['town'], id=workplace['id'])
+            joblisting.workplaces.add(workplace)
+        return recipe
