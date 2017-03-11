5fe20bc20e52c61855ad3e40d9012ce07da26349
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2017-03-11 01:02:00 +0100

    Initial commit for Quote comments

--- lego/apps/quotes/models.py
+++ lego/apps/quotes/models.py
@@ -1,5 +1,7 @@
+from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 
+from lego.apps.comments.models import Comment
 from lego.apps.content.models import Content
 from lego.utils.models import BasisModel
 
@@ -8,6 +10,7 @@ class Quote(BasisModel, Content):
 
     source = models.CharField(max_length=255)
     approved = models.BooleanField(default=False)
+    comments = GenericRelation(Comment)
 
     def __str__(self):
         return self.title
@@ -19,3 +22,9 @@ class Quote(BasisModel, Content):
     def unapprove(self):
         self.approved = False
         self.save()
+
+    @property
+    def comment_count(self):
+        print('Comments:')
+        print(self.comments)
+        return len(self.comments.objects.all())
