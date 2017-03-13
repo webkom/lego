47d02e4acda183020e1d29975adb3634497963a4
Hans-Wilhelm Warlo <hw@warlo.no>
2017-03-13 17:19:27 +0100

    Add permissions and a few tests

--- lego/apps/companies/views.py
+++ lego/apps/companies/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 
 from lego.apps.companies.models import Company, CompanyContact, SemesterStatus
+from lego.apps.companies.permissions import CompanyPermissions
 from lego.apps.companies.serializers import (CompanyContactCreateAndUpdateSerializer,
                                              CompanyContactReadSerializer,
                                              CompanyCreateAndUpdateSerializer,
@@ -8,16 +9,18 @@ from lego.apps.companies.serializers import (CompanyContactCreateAndUpdateSerial
                                              SemesterStatusCreateAndUpdateSerializer,
                                              SemesterStatusReadDetailedSerializer,
                                              SemesterStatusReadSerializer)
+from lego.apps.permissions.permissions import AbakusPermission
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all().prefetch_related('semester_statuses')
+    permission_classes = (CompanyPermissions,)
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
             return CompanyCreateAndUpdateSerializer
 
-        elif self.action in ['retrieve', 'destroy']:
+        elif self.action in ['retrieve']:
             return CompanyReadDetailedSerializer
 
         return CompanyReadSerializer
@@ -25,12 +28,13 @@ class CompanyViewSet(viewsets.ModelViewSet):
 
 class SemesterStatusViewSet(viewsets.ModelViewSet):
     queryset = SemesterStatus.objects.all()
+    permission_classes = (AbakusPermission,)
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
             return SemesterStatusCreateAndUpdateSerializer
 
-        elif self.action in ['retrieve', 'destroy']:
+        elif self.action in ['retrieve']:
             return SemesterStatusReadDetailedSerializer
 
         return SemesterStatusReadSerializer
@@ -45,6 +49,7 @@ class SemesterStatusViewSet(viewsets.ModelViewSet):
 
 class CompanyContactViewSet(viewsets.ModelViewSet):
     queryset = CompanyContact.objects.all()
+    permission_classes = (AbakusPermission,)
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
