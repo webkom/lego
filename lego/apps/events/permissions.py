d09e40e235d832c6d3052bcb17fe3dbf4cf6d77a
Hans-Wilhelm Warlo <hw@warlo.no>
2017-03-13 15:47:04 +0100

    Add waiting registrations and registration list endpoint for admins

--- lego/apps/events/permissions.py
+++ lego/apps/events/permissions.py
@@ -1,5 +1,6 @@
 from structlog import get_logger
 
+from lego.apps.events.models import Registration
 from lego.apps.permissions.permissions import AbakusPermission
 
 log = get_logger()
@@ -12,18 +13,27 @@ class PoolPermissions(AbakusPermission):
 class RegistrationPermissions(AbakusPermission):
     """
     This allows users to update and destroy the registration object.
-    Update is used to set new feedback and destroy is used for unregistration.
+    Update is used to set new feedback and destroy is used to unregister.
 
     Note: Users are given keyword permission to create (register), however they can only register
-    to events that are not filtered away i.e. we can still create events that are hidden
-    e.g "Helgesamling" that regular users cant register to.
+    to events that are not filtered away meaning we can still create events that are hidden
+    e.g. "Helgesamling" that regular users cant register to.
     """
     allowed_individual = ['retrieve', 'update', 'partial_update', 'destroy']
-    check_object_permission = True
 
-    def is_self(self, request, view, obj):
-        if view.action in self.allowed_individual and obj.user == request.user:
+    def is_self(self, request, view, obj=None):
+        if view.action in self.allowed_individual:
+            registration_id = view.kwargs.get('pk', None)
+            reg = Registration.objects.get(pk=registration_id)
+            if obj and obj.user == request.user:
+                return True
+            if reg and request.user.id == reg.user_id:
+                return True
+
+    def has_permission(self, request, view):
+        if self.is_self(request, view):
             return True
+        return super().has_permission(request, view)
 
     def has_object_permission(self, request, view, obj):
         if self.is_self(request, view, obj):
