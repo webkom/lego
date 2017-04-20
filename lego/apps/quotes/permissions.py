440d8a9fd463b970a7363c96b8ec63d6572f71bb
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-04-20 18:20:57 +0200

    All authenticated users should have access to the random quotes endpoint

--- lego/apps/quotes/permissions.py
+++ lego/apps/quotes/permissions.py
@@ -4,6 +4,7 @@ from lego.apps.permissions.permissions import AbakusPermission
 class QuotePermissions(AbakusPermission):
 
     permission_map = {
+        'random': [],
         'approve': ['/sudo/admin/quotes/change-approval/'],
         'unapprove': ['/sudo/admin/quotes/change-approval/'],
     }
