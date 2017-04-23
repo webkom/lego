268d6767d691cb4b781f269ce4d82707616257da
Kristoffer K Larsen <kristoffer@larsen.so>
2017-04-23 22:58:01 +0200

    Search fixes (#549)
    * Add LDAP settings
    
    * Small fixes for making autocomplete better
    
    * Add missing fixture
    
    * fix isort

--- lego/apps/flatpages/models.py
+++ lego/apps/flatpages/models.py
@@ -3,6 +3,7 @@ from mptt.fields import TreeForeignKey
 from mptt.models import MPTTModel
 
 from lego.apps.content.models import SlugModel
+from lego.apps.files.models import FileField
 from lego.utils.managers import TreeBasisManager
 from lego.utils.models import BasisModel
 
@@ -10,6 +11,7 @@ from lego.utils.models import BasisModel
 class Page(MPTTModel, BasisModel, SlugModel):
     title = models.CharField('title', max_length=200)
     content = models.TextField('content')
+    picture = FileField()
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
     slug_field = 'title'
 
