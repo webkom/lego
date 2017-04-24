14c2476c07e3149f6622b8950116eaad16a8227c
Odin Ugedal <odin@ugedal.com>
2017-04-24 19:25:14 +0200

    Fix meetings permission for users without auth (#548)

--- lego/apps/meetings/permissions.py
+++ lego/apps/meetings/permissions.py
@@ -8,6 +8,11 @@ class MeetingPermissions(AbakusPermission):
 
     force_object_permission_check = True
 
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        return super().has_permission(request, view)
+
     def has_object_permission(self, request, view, meeting):
         if super().has_object_permission(request, view, meeting):
             return True
