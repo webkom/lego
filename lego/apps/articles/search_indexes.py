57adcf84a3b4e51abd4cd12a25ca1b2f68eac8d3
Kristoffer K Larsen <kristoffer@larsen.so>
2017-05-13 14:34:27 +0200

    Article search serializer (#586)
    * Update serializers.py
    
    * Update search_indexes.py

--- lego/apps/articles/search_indexes.py
+++ lego/apps/articles/search_indexes.py
@@ -2,13 +2,13 @@ from lego.apps.search import register
 from lego.apps.search.index import SearchIndex
 
 from .models import Article
-from .serializers import DetailedArticleSerializer
+from .serializers import SearchArticleSerializer
 
 
 class ArticleModelIndex(SearchIndex):
 
     queryset = Article.objects.all()
-    serializer_class = DetailedArticleSerializer
+    serializer_class = SearchArticleSerializer
     result_fields = ('title', 'description', 'text', 'cover')
     autocomplete_result_fields = ('title')
 
