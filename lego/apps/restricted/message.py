29b8c0f9fc6b54c209bd685abd87ec850fd7b7d1
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-06-15 23:19:03 +0200

    Hide restricted recipients

--- lego/apps/restricted/message.py
+++ lego/apps/restricted/message.py
@@ -27,6 +27,9 @@ class EmailMessage:
     @staticmethod
     def extend_instance(obj, cls):
         """Apply mixin to a class instance after creation"""
+        if isinstance(obj, cls):
+            return obj
+
         base_cls = obj.__class__
         base_cls_name = obj.__class__.__name__
         obj.__class__ = type(base_cls_name, (cls, base_cls), {})
