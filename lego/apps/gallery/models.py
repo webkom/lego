e06d54ff3de85426108be74e610bbec9f3b06efe
larseen <kristoffer@larsen.so>
2017-07-27 22:38:12 +0200

    more gallery stuff

--- lego/apps/gallery/models.py
+++ lego/apps/gallery/models.py
@@ -14,6 +14,7 @@ class Gallery(BasisModel, ObjectPermissionsModel):
     """
     title = models.CharField(max_length=128)
     description = models.TextField(blank=True)
+    cover = models.ForeignKey('gallery.GalleryPicture', related_name='gallery_covers', null=True)
     location = models.CharField(max_length=64)
     taken_at = models.DateField(null=True)
     photographers = models.ManyToManyField('users.User')
