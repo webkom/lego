51c801dd17ef4483e85a005acb7c3b4c3ce11e80
Martin Hafskjold Thoresen <martinhath@gmail.com>
2017-08-29 12:28:50 +0200

    Allow for membership creation with the API

--- lego/apps/users/serializers/memberships.py
+++ lego/apps/users/serializers/memberships.py
@@ -4,16 +4,18 @@ from lego.apps.users.models import Membership
 from lego.apps.users.serializers.users import PublicUserSerializer
 
 
-class MembershipSerializer(serializers.ModelSerializer):
-    user = PublicUserSerializer(many=False, read_only=True)
-
+class MembershipCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Membership
         fields = (
             'user',
-            'abakus_group_id',
+            'abakus_group',
             'role',
             'is_active',
             'start_date',
             'end_date'
         )
+
+
+class MembershipSerializer(MembershipCreateSerializer):
+    user = PublicUserSerializer(many=False, read_only=True)
--- lego/apps/users/views/memberships.py
+++ lego/apps/users/views/memberships.py
@@ -3,11 +3,17 @@ from rest_framework import viewsets
 from lego.apps.permissions.api.views import AllowedPermissionsMixin
 from lego.apps.users.filters import MembershipFilterSet
 from lego.apps.users.models import Membership
-from lego.apps.users.serializers.memberships import MembershipSerializer
+from lego.apps.users.serializers.memberships import MembershipSerializer, MembershipCreateSerializer
 
 
 class MembershipViewSet(AllowedPermissionsMixin, viewsets.ModelViewSet):
     queryset = Membership.objects.all().select_related('user')
     serializer_class = MembershipSerializer
+    filter_backends = (AbakusObjectPermissionFilter, filters.DjangoFilterBackend,)
     filter_class = MembershipFilterSet
     ordering = 'id'
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return MembershipCreateSerializer
+        return MembershipSerializer
