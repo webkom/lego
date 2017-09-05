f1254891cf2adad21e3cc96fd0ae5bb35778f139
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-09-05 18:26:55 +0200

    Change user serializers for better update support, fixes #415
    Some imporvements on user registration and student verification.

--- lego/apps/users/serializers/registration.py
+++ lego/apps/users/serializers/registration.py
@@ -1,10 +1,17 @@
-from rest_framework import serializers
+from django.contrib.auth import password_validation
+from rest_framework import exceptions, serializers
 
 from lego.apps.users.models import User
+from lego.utils.functions import verify_captcha
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
     captcha_response = serializers.CharField(required=True)
+
+    def validate_captcha_response(self, captcha_response):
+        if not verify_captcha(captcha_response):
+            raise exceptions.ValidationError('invalid_captcha')
+        return captcha_response
 
     class Meta:
         model = User
@@ -13,6 +20,10 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
 class RegistrationConfirmationSerializer(serializers.ModelSerializer):
 
+    def validate_password(self, password):
+        password_validation.validate_password(password)
+        return password
+
     class Meta:
         model = User
         fields = (
@@ -20,6 +31,5 @@ class RegistrationConfirmationSerializer(serializers.ModelSerializer):
             'first_name',
             'last_name',
             'gender',
-            'email',
             'password'
         )
--- lego/apps/users/views/users.py
+++ lego/apps/users/views/users.py
@@ -1,41 +1,61 @@
 from rest_framework import status, viewsets
 from rest_framework.decorators import list_route
-from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from structlog import get_logger
 
 from lego.apps.permissions.api.views import AllowedPermissionsMixin
+from lego.apps.permissions.constants import EDIT
 from lego.apps.users import constants
 from lego.apps.users.models import AbakusGroup, User
 from lego.apps.users.serializers.registration import RegistrationConfirmationSerializer
-from lego.apps.users.serializers.users import DetailedUserSerializer, MeSerializer, UserSerializer
+from lego.apps.users.serializers.users import (DetailedUserSerializer, MeSerializer,
+                                               PublicUserSerializer)
 
 log = get_logger()
 
 
 class UsersViewSet(AllowedPermissionsMixin, viewsets.ModelViewSet):
+
     queryset = User.objects.all()
     lookup_field = 'username'
-    serializer_class = UserSerializer
+    serializer_class = PublicUserSerializer
     ordering = 'id'
+
+    def get_serializer_class(self):
+        """
+        Users should receive the DetailedUserSerializer if they tries to get themselves or have the
+        EDIT permission.
+        """
+        if self.action in ['retrieve', 'update', 'partial_update']:
+            instance = self.get_object()
+            if self.request.user.has_perm(EDIT, instance) or self.request.user == instance:
+                return DetailedUserSerializer
+
+        elif self.action == 'create':
+            return RegistrationConfirmationSerializer
+
+        return super().get_serializer_class()
 
     def get_permissions(self):
         """
-        Override permission classes as @permission_classes on methods
-        do not work with ModelViewSets.
+        The create action are used to register user, we do not require authentication on that
+        endpoint.
         """
-        if self.request.method == 'POST':
-            # Allow anyone to POST (i.e. visitors that are not registered)
-            self.permission_classes = [AllowAny, ]
+        if self.action == 'create':
+            return [AllowAny()]
 
-        return super(UsersViewSet, self).get_permissions()
+        return super().get_permissions()
 
     @list_route(
         methods=['GET'], permission_classes=[IsAuthenticated],
         serializer_class=MeSerializer
     )
     def me(self, request):
+        """
+        Read-only endpoint used to retrieve information about the authenticated user.
+        """
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
 
@@ -43,13 +63,6 @@ class UsersViewSet(AllowedPermissionsMixin, viewsets.ModelViewSet):
         """
         Attempts to register a new user based on the registration token.
         """
-        if request.user.is_authenticated():
-            # Authenticated users are not allowed to
-            # create / register new users unless they are an admin.
-            # TODO: add admin user registration.
-            log.warn('admin_registration_not_implemented')
-            raise PermissionDenied()
-
         if not request.GET.get('token', False):
             raise ValidationError(detail='Registration token is required.')
 
@@ -58,15 +71,12 @@ class UsersViewSet(AllowedPermissionsMixin, viewsets.ModelViewSet):
         if token_email is None:
             raise ValidationError(detail='Token expired or invalid.')
 
-        serializer = RegistrationConfirmationSerializer(data={
-            **request.data,
-            'email': token_email,
-        })
-
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
-        new_user = User.objects.create_user(**serializer.validated_data)
+        user = User.objects.create(**{
+            **serializer.validated_data,
+            'email': token_email
+        })
         user_group = AbakusGroup.objects.get(name=constants.USER_GROUP)
-        user_group.add_user(new_user)
-
-        return Response(DetailedUserSerializer(new_user).data, status=status.HTTP_201_CREATED)
+        user_group.add_user(user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
