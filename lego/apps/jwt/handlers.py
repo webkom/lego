ba828f3c8dd763a15a6a21b931ae4872a51816e0
Hans-Wilhelm Warlo <hw@warlo.no>
2017-09-06 22:45:55 +0200

    Use correct create_user to enable hashing, fix password in serializer and jwt on user creation

--- lego/apps/jwt/handlers.py
+++ lego/apps/jwt/handlers.py
@@ -1,4 +1,9 @@
+from rest_framework_jwt.settings import api_settings
+
 from lego.apps.users.serializers.users import MeSerializer
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 
 def response_handler(token, user=None, request=None):
@@ -6,3 +11,9 @@ def response_handler(token, user=None, request=None):
         'token': token,
         'user': MeSerializer(user).data
     }
+
+
+def get_jwt_token(user):
+    payload = jwt_payload_handler(user)
+    token = jwt_encode_handler(payload)
+    return response_handler(token, user)
