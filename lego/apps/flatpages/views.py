30a749c5c610216094a1af306595f105eb96072e
Odin Ugedal <odin@ugedal.com>
2017-09-08 00:17:53 +0200

    Add initial support for tree view for flatpages

--- lego/apps/flatpages/views.py
+++ lego/apps/flatpages/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import decorators, viewsets
+from rest_framework.response import Response
+
+from itertools import chain
 
 from lego.apps.permissions.api.views import AllowedPermissionsMixin
-
 from .models import Page
 from .serializers import PageDetailSerializer, PageListSerializer
 
@@ -10,6 +12,26 @@ class PageViewSet(AllowedPermissionsMixin, viewsets.ModelViewSet):
     queryset = Page.objects.all()
     order_by = 'created_at'
     lookup_field = 'slug'
+
+    @decorators.detail_route(methods=['GET'])
+    def tree(self, request, *args, **kwargs):
+        page = self.get_object()
+        siblings = page.get_siblings()
+        children = page.get_children()
+        parent = page.parent
+
+        result = chain(
+            PageListSerializer(children, many=True).data,
+            PageListSerializer(siblings, many=True).data,
+            PageDetailSerializer([page], many=True).data
+        )
+
+        if parent:
+            result = chain(
+                result,
+                PageListSerializer([parent], many=True).data
+            )
+        return Response(result)
 
     def get_serializer_class(self):
         if self.action == 'list':
