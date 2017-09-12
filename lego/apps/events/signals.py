7f5171e7a579cd0bd0c20d4db0c2cee53ba62f52
Hans-Wilhelm Warlo <hw@warlo.no>
2017-09-12 19:07:31 +0200

    Remove websocket call on deletion and enable unlimited pool

--- lego/apps/events/signals.py
+++ lego/apps/events/signals.py
@@ -2,5 +2,5 @@ from lego.apps.events.websockets import notify_event_updated
 
 
 def event_save_callback(sender, instance, created, **kwargs):
-    if not created:
+    if not created and not instance.deleted:
         notify_event_updated(instance)
