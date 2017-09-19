1f22a9ac71d46b70b3d0730b70208ba26c5dcf2f
kotlarz <marius.kotlarz@gmail.com>
2017-09-19 16:10:58 +0200

    Add missing roles, needed for migration from NERD

--- lego/apps/users/constants.py
+++ lego/apps/users/constants.py
@@ -12,12 +12,46 @@ MEMBER = 'member'
 LEADER = 'leader'
 CO_LEADER = 'co-leader'
 TREASURER = 'treasurer'
+RECRUITING = 'recruiting'
+DEVELOPMENT = 'development'
+EDITOR = 'editor'
+RETIREE = 'retiree'
+MEDIA_RELATIONS = 'media relations'
+ACTIVE_RETIREE = 'active retiree'
+ALUMNI = 'alumni'
+WEBMASTER = 'webmaster'
+INTEREST_GROUP_ADMIN = 'interest group admin'
+ALUMNI_ADMIN = 'alumni admin'
+VOTE_COUNTER = 'vote counter'
+RETIREE_EMAIL = 'retiree email'
+COMPANY_ADMIN = 'company admin'
+DUGNAD_ADMIN = 'dugnad admin'
+TRIP_ADMIN = 'trip admin'
+SPONSOR_ADMIN = 'sponsor admin'
+SOCIAL_ADMIN = 'social admin'
 
 ROLES = (
     (MEMBER, MEMBER),
     (LEADER, LEADER),
     (CO_LEADER, CO_LEADER),
-    (TREASURER, TREASURER)
+    (TREASURER, TREASURER),
+    (RECRUITING, RECRUITING),
+    (DEVELOPMENT, DEVELOPMENT),
+    (EDITOR, EDITOR),
+    (RETIREE, RETIREE),
+    (MEDIA_RELATIONS, MEDIA_RELATIONS),
+    (ACTIVE_RETIREE, ACTIVE_RETIREE),
+    (ALUMNI, ALUMNI),
+    (WEBMASTER, WEBMASTER),
+    (INTEREST_GROUP_ADMIN, INTEREST_GROUP_ADMIN),
+    (ALUMNI_ADMIN, ALUMNI_ADMIN),
+    (VOTE_COUNTER, VOTE_COUNTER),
+    (RETIREE_EMAIL, RETIREE_EMAIL),
+    (COMPANY_ADMIN, COMPANY_ADMIN),
+    (DUGNAD_ADMIN, DUGNAD_ADMIN),
+    (TRIP_ADMIN, TRIP_ADMIN),
+    (SPONSOR_ADMIN, SPONSOR_ADMIN),
+    (SOCIAL_ADMIN, SOCIAL_ADMIN)
 )
 
 DATA = 'data'
