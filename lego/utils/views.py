cfd4491fcd13c71b549928e804489a25866dbed4
Martin Ek <mail@ekmartin.com>
2017-09-29 00:21:58 +0200

    Add anonymous entities to meta endpoint

--- lego/utils/views.py
+++ lego/utils/views.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 from lego.apps.companies.models import Company
 from lego.apps.email.models import EmailList
 from lego.apps.gallery.models import Gallery
-from lego.apps.joblistings.models import Joblisting
 from lego.apps.meetings.models import Meeting
 from lego.apps.notifications.models import Announcement
 from lego.apps.permissions.constants import CREATE, LIST
@@ -21,22 +20,31 @@ class SiteMetaViewSet(viewsets.ViewSet):
         user = request.user
         site_meta = settings.SITE
 
-        entities = {
+        # Allow non-logged in users to see these as well:
+        allow_anonymous_entities = [
+            'events',
+            'articles',
+            'joblistings',
+        ]
+
+        # Whereas these require that a user has keyword permissions:
+        permission_entities = {
             'companies': (Company, LIST),
-            'joblistings': (Joblisting, LIST),
             'meetings': (Meeting, LIST),
             'quotes': (Quote, LIST),
             'galleries': (Gallery, LIST),
+            'interest_groups': (AbakusGroup, LIST),
 
             # Admin:
+            'bdb': (Company, CREATE),
             'announcements': (Announcement, CREATE),
             'groups': (AbakusGroup, CREATE),
             'email': (EmailList, CREATE),
             'users': (User, CREATE),
         }
 
-        is_allowed = {}
-        for entity, (model, permission) in entities.items():
+        is_allowed = {entity: True for entity in allow_anonymous_entities}
+        for entity, (model, permission) in permission_entities.items():
             is_allowed[entity] = user.has_perm(permission, model)
 
         return Response({
