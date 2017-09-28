c643e36dc8a73022166e375d676ef85963731c47
Martin Hafskjold Thoresen <martinhath@gmail.com>
2017-09-29 01:20:24 +0200

    Fix MembershipSerializer, which is supposed to return the entire user

--- lego/apps/users/serializers/memberships.py
+++ lego/apps/users/serializers/memberships.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
-from lego.apps.users.models import AbakusGroup, Membership
+from lego.apps.users.fields import PublicUserField
+from lego.apps.users.models import AbakusGroup, Membership, User
 
 
 class MembershipSerializer(serializers.ModelSerializer):
+    user = PublicUserField(queryset=User.objects.all())
+
     class Meta:
         model = Membership
         fields = (
