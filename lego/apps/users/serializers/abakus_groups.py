9a63981377a61e8d6eb3ccb10c53b918c7901366
Martin Hafskjold Thoresen <martinhath@gmail.com>
2017-10-12 19:34:57 +0200

    Make the creator of a group the leader of that group

--- lego/apps/users/serializers/abakus_groups.py
+++ lego/apps/users/serializers/abakus_groups.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 from lego.apps.files.fields import ImageField
-from lego.apps.users.models import AbakusGroup
+from lego.apps.users import constants
+from lego.apps.users.models import AbakusGroup, Membership
 from lego.apps.users.serializers.memberships import MembershipSerializer
 
 
@@ -22,6 +23,16 @@ class DetailedAbakusGroupSerializer(serializers.ModelSerializer):
             'text',
             'logo',
         )
+
+    def create(self, validated_data):
+        group = super(DetailedAbakusGroupSerializer, self).create(validated_data)
+        user = self.context['request'].user
+        Membership.objects.create(**{
+            'user': user,
+            'abakus_group': group,
+            'role': constants.LEADER,
+        })
+        return group
 
 
 class PublicAbakusGroupSerializer(serializers.ModelSerializer):
