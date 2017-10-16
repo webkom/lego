a9d86fadf912915b250a5ede9f5a8464c0776d22
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-10-16 10:41:37 +0200

    Change password-reset templates, plain <-> html

--- lego/apps/users/views/password_reset.py
+++ lego/apps/users/views/password_reset.py
@@ -34,8 +34,8 @@ class PasswordResetRequestViewSet(viewsets.GenericViewSet):
                 "token": token,
             },
             subject='Nullstill ditt passord på abakus.no',
-            plain_template='users/email/reset_password.html',
-            html_template='users/email/reset_password.txt',
+            plain_template='users/email/reset_password.txt',
+            html_template='users/email/reset_password.html',
             from_email=None
         )
 
