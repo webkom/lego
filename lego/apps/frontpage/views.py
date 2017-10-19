c19e9581e1bbdfbf08089d8f1615ec5fe9b520a5
Martin Hafskjold Thoresen <martinhath@gmail.com>
2017-10-19 23:20:01 +0200

    Filter out old events, and make new articles appear first

--- lego/apps/frontpage/views.py
+++ lego/apps/frontpage/views.py
@@ -1,3 +1,4 @@
+from django.utils.datetime_safe import datetime
 from rest_framework import viewsets, serializers
 from rest_framework.response import Response
 
@@ -10,11 +11,8 @@ from lego.apps.events.serializers.events import EventSearchSerializer
 class FrontpageViewSet(viewsets.ViewSet):
 
     def list(self, _request):
-        # TODO: filter out old events
-        # note that articles will be old, but events are 'new', in the sense
-        # that the start time hasn't passed yet.
-        queryset_articles = Article.objects.all().order_by('pinned', 'created_at')
-        queryset_events = Event.objects.all().order_by('pinned', 'start_time')
+        queryset_articles = Article.objects.all().order_by('pinned', '-created_at')
+        queryset_events = Event.objects.all().filter(start_time__gt=datetime.now()).order_by('pinned', 'start_time')
         articles = SearchArticleSerializer(queryset_articles, many=True).data
         events = EventSearchSerializer(queryset_events, many=True).data
         ret = {
