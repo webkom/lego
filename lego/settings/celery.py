a7917e8394c74bea1413733f4bf5d33a20bcec24
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-10-20 04:23:50 +0200

    Run external sync every hour, takes some time to complete
    Every 15 mins id to often.

--- lego/settings/celery.py
+++ lego/settings/celery.py
@@ -74,7 +74,7 @@ schedule = {
     },
     'sync-external-systems': {
         'task': 'lego.apps.external_sync.tasks.sync_external_systems',
-        'schedule': crontab(minute='*/15')
+        'schedule': crontab(hour='*/1')
     },
     'check-that-pool-counters-match-registration-number': {
         'task': 'lego.apps.events.tasks.check_that_pool_counters_match_registration_number',
