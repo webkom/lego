823d37480f06237dc75bd08725b2f8d69d06e21a
larseen <kristoffer@larsen.so>
2018-02-19 20:20:10 +0100

    Add more tags in html whitelist

--- lego/apps/content/utils.py
+++ lego/apps/content/utils.py
@@ -7,14 +7,16 @@ def sanitize_html(value, allow_images=True):
     """
     tags = [
         'p', 'b', 'i', 'u', 'h1', 'h2', 'code', 'pre', 'blockquote', 'strong', 'strike', 'ul',
-        'cite', 'li', 'em', 'hr', 'div', 'a'
+        'cite', 'li', 'em', 'hr', 'div', 'a', 'figure', 'figcaption', 'input', 'ol'
     ]
     if allow_images:
         tags.append('img')
 
     safe_content = bleach.clean(
         value, tags=tags, attributes={
-            'a': ['href'],
+            'div': ['data-type'],
+            'a': ['href', 'target', 'rel'],
+            'input': ['type', 'disabled', 'checked'],
             'img': ['data-file-key'],
         }, strip=True, strip_comments=True
     )
