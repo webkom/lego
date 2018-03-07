13df8fffed38de6f6b6d376412f7535f47102f5a
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2018-03-07 15:50:32 +0100

    Remove cassandra build settings

--- lego/settings/production.py
+++ lego/settings/production.py
@@ -16,7 +16,7 @@ from lego.settings import (
 from .secure import *  # noqa
 
 env = environ.Env(
-    DEBUG=(bool, False), ALLOWED_HOSTS=(list, ['api.abakus.no']), CASSANDRA_HOSTS=(list, [])
+    DEBUG=(bool, False), ALLOWED_HOSTS=(list, ['api.abakus.no'])
 )
 environ.Env.read_env(os.path.join(os.path.dirname(BASE_DIR), '.env'))
 
