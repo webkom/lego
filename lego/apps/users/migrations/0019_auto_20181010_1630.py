6eb3a6578f988dd07b23a0bc582d4d1a03786342
Odin Ugedal <odin@ugedal.com>
2018-10-11 19:39:45 +0200

    Fix migrations

--- lego/apps/users/migrations/0019_auto_20181010_1630.py
+++ lego/apps/users/migrations/0019_auto_20181010_1630.py
@@ -2,6 +2,7 @@
 
 import django.contrib.postgres.fields
 from django.db import migrations, models
+
 import lego.apps.permissions.validators
 
 
@@ -15,6 +16,11 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='abakusgroup',
             name='permissions',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=50, validators=[lego.apps.permissions.validators.KeywordPermissionValidator()]), default=list, size=None, verbose_name='permissions'),
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(
+                    max_length=50,
+                    validators=[lego.apps.permissions.validators.KeywordPermissionValidator()]
+                ), default=list, size=None, verbose_name='permissions'
+            ),
         ),
     ]
