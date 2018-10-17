d3665a9bad5296355edf88fb4f9b138064dd7012
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2018-10-17 23:33:57 +0200

    Improve contact form tests

--- lego/apps/contact/tests/test_send.py
+++ lego/apps/contact/tests/test_send.py
@@ -6,6 +6,13 @@ from lego.apps.contact.send import send_message
 from lego.apps.users.models import User
 from lego.utils.test_utils import BaseTestCase
 
+default_values = {
+    'from_email': None,
+    'html_template': 'contact/email/contact_form.html',
+    'plain_template': 'contact/email/contact_form.txt',
+    'subject': 'Ny henvendelse fra kontaktskjemaet'
+}
+
 
 class SendTestCase(BaseTestCase):
 
@@ -16,15 +23,72 @@ class SendTestCase(BaseTestCase):
 
     @mock.patch('lego.apps.contact.send.send_email.delay')
     def test_send_anonymous(self, mock_send_email):
-        send_message('title', 'message', AnonymousUser(), True)
+        """
+        Send in a contact form as not logged in user, set to be anonymous
+        """
+        anonymus_user = AnonymousUser()
+
+        send_message('title', 'message', anonymus_user, True)
+        mock_send_email.assert_called_with(
+            to_email="hs@abakus.no", context={
+                'title': 'title',
+                'message': 'message',
+                'from_name': "Anonymous",
+                'from_email': "Unknown"
+            }, **default_values
+        )
         mock_send_email.assert_called_once()
 
     @mock.patch('lego.apps.contact.send.send_email.delay')
     def test_send_anonymous_user(self, mock_send_email):
-        send_message('title', 'message', AnonymousUser(), False)
+        """
+        Send in a contact form as not logged in user
+        """
+        anonymus_user = AnonymousUser()
+
+        send_message('title', 'message', anonymus_user, False)
+        mock_send_email.assert_called_with(
+            to_email="hs@abakus.no", context={
+                'title': 'title',
+                'message': 'message',
+                'from_name': "Anonymous",
+                'from_email': "Unknown"
+            }, **default_values
+        )
         mock_send_email.assert_called_once()
 
     @mock.patch('lego.apps.contact.send.send_email.delay')
     def test_send_user(self, mock_send_email):
-        send_message('title', 'message', User.objects.first(), False)
+        """
+        Send in a contact form as logged in user, showing name
+        """
+        logged_in_user = User.objects.first()
+
+        send_message('title', 'message', logged_in_user, False)
+        mock_send_email.assert_called_with(
+            to_email="hs@abakus.no", context={
+                'title': 'title',
+                'message': 'message',
+                'from_name': logged_in_user.full_name,
+                'from_email': logged_in_user.email_address
+            }, **default_values
+        )
+        mock_send_email.assert_called_once()
+
+    @mock.patch('lego.apps.contact.send.send_email.delay')
+    def test_send_user_set_anonymous(self, mock_send_email):
+        """
+        Send in a contact form as logged in user, set to be anonymous
+        """
+        logged_in_user = User.objects.first()
+
+        send_message('title', 'message', logged_in_user, True)
+        mock_send_email.assert_called_with(
+            to_email="hs@abakus.no", context={
+                'title': 'title',
+                'message': 'message',
+                'from_name': "Anonymous",
+                'from_email': "Unknown"
+            }, **default_values
+        )
         mock_send_email.assert_called_once()
