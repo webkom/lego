ea7cf03464851d2ddadad5db21fcdd98e6c71a4a
Ludvig Hozman <ludvig123@gmail.com>
2019-02-11 20:46:57 +0100

    Add emailAddress to public user serializer

--- lego/apps/users/serializers/users.py
+++ lego/apps/users/serializers/users.py
@@ -60,6 +60,7 @@ class PublicUserSerializer(serializers.ModelSerializer):
             "full_name",
             "gender",
             "profile_picture",
+            "email_address",
         )
         read_only_fields = ("username",)
 
