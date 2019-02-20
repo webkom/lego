c05f01ba3146cec19eedb10d6db44d0c0a82fb89
Martin Farstad <farstad2k@gmail.com>
2019-02-20 20:26:19 +0100

    Add youtube_url field in article model

--- lego/apps/articles/models.py
+++ lego/apps/articles/models.py
@@ -1,13 +1,18 @@
 from django.conf import settings
+from django.db.models import URLField
+from django.core.validators import URLValidator
 
 from lego.apps.content.models import Content
 from lego.apps.files.models import FileField
 from lego.apps.permissions.models import ObjectPermissionsModel
 from lego.utils.models import BasisModel
 
+from rest_framework.exceptions import ValidationError
+
 
 class Article(Content, BasisModel, ObjectPermissionsModel):
     cover = FileField(related_name="article_covers")
+    youtube_url = URLField(default="")
 
     class Meta:
         abstract = False
