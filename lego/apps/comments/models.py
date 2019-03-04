de49c3498a8a501676fc82d8d188bbc695f05757
Ludvig Hozman <ludvig123@gmail.com>
2019-03-04 20:19:56 +0100

    Add logic for deleting comments

--- lego/apps/comments/models.py
+++ lego/apps/comments/models.py
@@ -1,6 +1,6 @@
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-from django.db import models
+from django.db import models, transaction
 
 from lego.apps.comments.permissions import CommentPermissionHandler
 from lego.apps.content.fields import ContentField
@@ -15,7 +15,7 @@ class CommentManager(BasisModelManager):
 
 class Comment(BasisModel):
 
-    text = ContentField(allow_images=False)
+    text = ContentField(allow_images=False, null=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField(db_index=True)
     content_object = GenericForeignKey()
@@ -26,6 +26,14 @@ class Comment(BasisModel):
     class Meta:
         ordering = ("created_at",)
         permission_handler = CommentPermissionHandler()
+
+    def delete(self):
+        if Comment.objects.filter(parent=self).exists():
+            self.created_by = None
+            self.text = None
+            self.save()
+        else:
+            super(Comment, self).delete(force=True)
 
     def __str__(self):
         return self.text
