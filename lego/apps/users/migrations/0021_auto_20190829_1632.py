2726eccf8cb64bf6a20f587d8e2ca913c96e217f
erlingfn <erling.nesset@gmail.com>
2019-08-29 18:54:31 +0200

    Format migration

--- lego/apps/users/migrations/0021_auto_20190829_1632.py
+++ lego/apps/users/migrations/0021_auto_20190829_1632.py
@@ -5,19 +5,17 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('users', '0020_abakusgroup_show_badge'),
-    ]
+    dependencies = [("users", "0020_abakusgroup_show_badge")]
 
     operations = [
         migrations.AlterField(
-            model_name='user',
-            name='first_name',
-            field=models.CharField(max_length=50, verbose_name='first name'),
+            model_name="user",
+            name="first_name",
+            field=models.CharField(max_length=50, verbose_name="first name"),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='last_name',
-            field=models.CharField(max_length=30, verbose_name='last name'),
+            model_name="user",
+            name="last_name",
+            field=models.CharField(max_length=30, verbose_name="last name"),
         ),
     ]
