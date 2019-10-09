e5184e527dce5f4973234abc2cd0bbaa5040da89
Peder Smith <smith.peder@gmail.com>
2019-10-09 20:12:36 +0200

    Revert-Workflows

--- lego/settings/__init__.py
+++ lego/settings/__init__.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-TESTING = "test" in sys.argv[:2] or os.environ.get("GITHUB_ACTIONS") is True
+TESTING = "test" in sys.argv[:2]
 DAPHNE_SERVER = "daphne" in sys.argv
 
 from .base import *  # noqa
