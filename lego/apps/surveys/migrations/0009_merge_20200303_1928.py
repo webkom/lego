93047fd2a1e012575894e8bf8b97910e810d6a7f
Finn Julius Stephansen-Smith <finni-94@hotmail.com>
2020-03-03 20:31:51 +0100

    Fix automerge migration linting

--- lego/apps/surveys/migrations/0009_merge_20200303_1928.py
+++ lego/apps/surveys/migrations/0009_merge_20200303_1928.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('surveys', '0008_auto_20200212_1821'),
-        ('surveys', '0008_question_display_type'),
+        ("surveys", "0008_auto_20200212_1821"),
+        ("surveys", "0008_question_display_type"),
     ]
 
-    operations = [
-    ]
+    operations = []
