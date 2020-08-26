a7963affcb2f9f46e3206f3d798ec4cd1b1dda6a
theodorsm <theodor@midtlien.com>
2020-08-27 01:34:45 +0200

    Add Personvern to flatpages

--- lego/apps/flatpages/constants.py
+++ lego/apps/flatpages/constants.py
@@ -2,10 +2,12 @@ GENERAL = "generelt"
 COMPANIES = "bedrifter"
 EVENTS = "arrangementer"
 GROUPS = "grupper"
+PRIVACY = "personvern"
 
 CATEGORIES = (
     (GENERAL, GENERAL),
     (COMPANIES, COMPANIES),
     (EVENTS, EVENTS),
     (GROUPS, GROUPS),
+    (PRIVACY, PRIVACY),
 )
