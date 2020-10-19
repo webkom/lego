819b1fdd2d14bb8e8dd080442cef2d242979c064
Odin Ugedal <odin@uged.al>
2020-10-19 12:11:34 +0200

    Update filtering to work properly

--- lego/apps/email/filters.py
+++ lego/apps/email/filters.py
@@ -1,3 +1,5 @@
+from django.db.models import F, Value
+from django.db.models.functions import Concat
 from django_filters.rest_framework import BooleanFilter, CharFilter, FilterSet
 
 from lego.apps.email.models import EmailList
@@ -7,31 +9,28 @@ from lego.apps.users.models import User
 class EmailListFilterSet(FilterSet):
 
     name = CharFilter(lookup_expr="icontains")
-    email = CharFilter(field_name="email__email", lookup_expr="icontains")
-    userFirstName = CharFilter(
-        field_name="internal_email__user__last_name", lookup_expr="icontains"
-    )
-    userLastName = CharFilter(
-        field_name="internal_email__user__first_name", lookup_expr="icontains"
-    )
-    userUsername = CharFilter(field_name="user__username", lookup_expr="icontains")
+    email = CharFilter(lookup_expr="icontains", field_name="email__email")
+    requireInternalAddress = BooleanFilter(field_name="require_internal_address")
 
     class Meta:
         model = EmailList
-        fields = ("name", "email", "userFirstName", "userLastName", "userUsername")
+        fields = ("name", "email", "requireInternalAddress")
 
 
 class EmailUserFilterSet(FilterSet):
 
     email = CharFilter(field_name="internal_email__email", lookup_expr="icontains")
-    userFirstName = CharFilter(
-        field_name="internal_email__user__last_name", lookup_expr="icontains"
-    )
-    userLastName = CharFilter(
-        field_name="internal_email__user__first_name", lookup_expr="icontains"
-    )
+    userUsername = CharFilter(field_name="username", lookup_expr="icontains")
+    userFullname = CharFilter(field_name="userFullname", method="fullname")
     enabled = BooleanFilter(field_name="internal_email_enabled")
+
+    def fullname(self, queryset, name, value):
+        if value:
+            return queryset.annotate(
+                fullname=Concat(F("first_name"), Value(" "), F("last_name"))
+            ).filter(fullname__icontains=value)
+        return queryset
 
     class Meta:
         model = User
-        fields = ("email", "enabled", "userFirstName", "userLastName")
+        fields = ("email", "enabled", "userUsername", "userFullname")
--- lego/apps/users/filters.py
+++ lego/apps/users/filters.py
@@ -1,26 +1,35 @@
-from django_filters import CharFilter, FilterSet
+from django.db.models import F, Value
+from django.db.models.functions import Concat
+from django_filters.rest_framework import CharFilter, FilterSet
 
 from lego.apps.users.models import AbakusGroup, Membership, MembershipHistory, Penalty
 
 
 class MembershipFilterSet(FilterSet):
 
-    userFirstName = CharFilter(field_name="user__last_name", lookup_expr="icontains")
-    userLastName = CharFilter(field_name="user__first_name", lookup_expr="icontains")
     userUsername = CharFilter(field_name="user__username", lookup_expr="icontains")
+    userFullname = CharFilter(field_name="userFullname", method="user__fullname")
     abakusGroupName = CharFilter(
         field_name="abakus_group__name", lookup_expr="icontains"
     )
 
     class Meta:
         model = Membership
-        fields = [
+        fields = (
             "role",
-            "userFirstName",
-            "userLastName",
             "userUsername",
             "abakusGroupName",
-        ]
+            "userFullname",
+        )
+
+    def user__fullname(self, queryset, name, value):
+        if value:
+            return queryset.annotate(
+                user__fullname=Concat(
+                    F("user__first_name"), Value(" "), F("user__last_name")
+                )
+            ).filter(user__fullname__icontains=value)
+        return queryset
 
 
 class AbakusGroupFilterSet(FilterSet):
