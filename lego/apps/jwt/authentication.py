98eaac727f245e363cf5f3e8eedab831d010a0ab
theodorsm <theodor@midtlien.com>
2021-02-24 14:36:45 +0100

    Fix isort

--- lego/apps/jwt/authentication.py
+++ lego/apps/jwt/authentication.py
@@ -1,5 +1,6 @@
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from django.contrib.auth.models import update_last_login
+
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from structlog import get_logger
 
 log = get_logger()
