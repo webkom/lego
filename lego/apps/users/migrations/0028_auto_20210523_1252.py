1fe8fec58d18d814a6b41179999e97e2c757ba8e
Mathias Oterhals Myklebust <mathias@oterbust.no>
2021-05-23 16:32:05 +0200

    Black reformat

--- lego/apps/users/migrations/0028_auto_20210523_1252.py
+++ lego/apps/users/migrations/0028_auto_20210523_1252.py
@@ -6,13 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0027_auto_20210415_1004'),
+        ("users", "0027_auto_20210415_1004"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='user',
-            name='selected_theme',
-            field=models.CharField(choices=[('auto', 'auto'), ('light', 'light'), ('dark', 'dark')], default='auto', max_length=50, verbose_name='selected theme'),
+            model_name="user",
+            name="selected_theme",
+            field=models.CharField(
+                choices=[("auto", "auto"), ("light", "light"), ("dark", "dark")],
+                default="auto",
+                max_length=50,
+                verbose_name="selected theme",
+            ),
         ),
     ]
