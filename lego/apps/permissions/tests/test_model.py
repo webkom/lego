2442c5dc8f3d4c3b24db5e12af6e29ab1fe75ddd
Ludvig Hozman <ludvig123@gmail.com>
2021-12-03 12:03:14 +0100

    Edit tests for django 3.2 changes
    * Equality on `Field` was changed in django 3.2 to make fields
      comparable across models.
      (https://github.com/django/django/commit/5cbfb48b92cb26a335f9c8c0f79d3390290103e2)
    * Instantiating AbstractModels now gives a TypeError. Edits tests to
      use a simple wrapping model around directly tested AbstractModels

--- lego/apps/permissions/tests/test_model.py
+++ lego/apps/permissions/tests/test_model.py
@@ -8,6 +8,15 @@ from lego.utils.test_utils import BaseTestCase
 class ObjectPermissionsModelTestCase(BaseTestCase):
     fixtures = ["test_abakus_groups.yaml", "test_users.yaml"]
 
+    def assertFieldsEqual(self, expected, actual):
+        expected_deconstructed = set(
+            [":".join(field.deconstruct()[0:2]) for field in expected]
+        )
+        actual_deconstructed = set(
+            [":".join(field.deconstruct()[0:2]) for field in actual]
+        )
+        self.assertSetEqual(expected_deconstructed, actual_deconstructed)
+
     def setUp(self):
         self.regular_users = User.objects.all()
         self.creator = self.regular_users[0]
@@ -18,10 +27,10 @@ class ObjectPermissionsModelTestCase(BaseTestCase):
     def test_inheritance(self):
         mixin_fields = set(ObjectPermissionsModel._meta.get_fields())
         test_fields = set(TestModel._meta.get_fields())
-        correct_fields = mixin_fields.union(test_fields)
-        fields = set(TestModel._meta.get_fields())
+        correct_fields = list(mixin_fields.union(test_fields))
+        fields = TestModel._meta.get_fields()
 
-        self.assertEqual(correct_fields, fields)
+        self.assertFieldsEqual(correct_fields, fields)
 
     def test_can_edit_users(self):
         can_edit_user = self.regular_users[1]
