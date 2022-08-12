7b0f83d06994cf6edeabb09bbe6cb0c9c9fe003d
erlingfn <erling.nesset@gmail.com>
2022-08-12 22:04:54 +0200

    Format migration

--- lego/apps/flatpages/migrations/0010_alter_page_category.py
+++ lego/apps/flatpages/migrations/0010_alter_page_category.py
@@ -6,13 +6,26 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('flatpages', '0009_auto_20200915_0823'),
+        ("flatpages", "0009_auto_20200915_0823"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='page',
-            name='category',
-            field=models.CharField(choices=[('generelt', 'generelt'), ('organisasjon', 'organisasjon'), ('styrer', 'styrer'), ('bedrifter', 'bedrifter'), ('arrangementer', 'arrangementer'), ('grupper', 'grupper'), ('utnevnelser', 'utnevnelser'), ('personvern', 'personvern')], default='generelt', max_length=50),
+            model_name="page",
+            name="category",
+            field=models.CharField(
+                choices=[
+                    ("generelt", "generelt"),
+                    ("organisasjon", "organisasjon"),
+                    ("styrer", "styrer"),
+                    ("bedrifter", "bedrifter"),
+                    ("arrangementer", "arrangementer"),
+                    ("grupper", "grupper"),
+                    ("utnevnelser", "utnevnelser"),
+                    ("personvern", "personvern"),
+                ],
+                default="generelt",
+                max_length=50,
+            ),
         ),
     ]
