d38ab7e228059bb3e866b09dc9066a5d96eda7f7
Erling Nesset <erling.nesset@gmail.com>
2022-08-12 22:17:42 +0200

    Add a few additional categories for flatpages (#2840)
    * Add a few additional categories for flatpages

--- lego/apps/flatpages/constants.py
+++ lego/apps/flatpages/constants.py
@@ -1,13 +1,19 @@
 GENERAL = "generelt"
+ORGANISATION = "organisasjon"
+BOARDS = "styrer"
 COMPANIES = "bedrifter"
 EVENTS = "arrangementer"
 GROUPS = "grupper"
+AWARDS = "utnevnelser"
 PRIVACY = "personvern"
 
 CATEGORIES = (
     (GENERAL, GENERAL),
+    (ORGANISATION, ORGANISATION),
+    (BOARDS, BOARDS),
     (COMPANIES, COMPANIES),
     (EVENTS, EVENTS),
     (GROUPS, GROUPS),
+    (AWARDS, AWARDS),
     (PRIVACY, PRIVACY),
 )
