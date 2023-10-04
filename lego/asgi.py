c3e2e1983c7d906b49cc9a4a6e0ff6fc8bee2d82
theodorsm <theodor@midtlien.com>
2023-10-05 01:15:04 +0200

    Test CI

--- lego/asgi.py
+++ lego/asgi.py
@@ -6,4 +6,6 @@ from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lego.settings")
 django.setup()
+
+## test
 application = get_default_application()
