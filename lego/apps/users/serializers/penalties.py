c7d06750fc73a394137cbcb3b5c442f45caa7630
Ivar Nakken <ivarnakken1441@gmail.com>
2024-01-21 21:43:51 +0100

    Serialize penalty's event field to a public event

--- lego/apps/users/serializers/penalties.py
+++ lego/apps/users/serializers/penalties.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
+from lego.apps.events.fields import PublicEventField
+from lego.apps.events.models import Event
 from lego.apps.users.models import Penalty
 
 
 class PenaltySerializer(serializers.ModelSerializer):
+    source_event = PublicEventField(queryset=Event.objects.all(), required=False)
+
     class Meta:
         model = Penalty
         fields = (
