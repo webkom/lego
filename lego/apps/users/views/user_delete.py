8862583e273911bb7e7de52dbfa7de10dd9ec3a5
christiangryt <dodif380@gmail.com>
2024-09-03 22:11:57 +0200

    prevent deletion with penalty

--- lego/apps/users/views/user_delete.py
+++ lego/apps/users/views/user_delete.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions, status, viewsets
+from rest_framework import permissions, status, viewsets,serializers
 from rest_framework.response import Response
 
 from lego.apps.users.serializers.user_delete import UserDeleteSerializer
@@ -12,5 +12,7 @@ class UserDeleteViewSet(viewsets.ViewSet):
             data=request.data, context={"request": request}
         )
         serializer.is_valid(raise_exception=True)
+        if self.request.user.number_of_penalties() > 0:
+            raise serializers.ValidationError("Has penalties")
         self.request.user.delete(force=True)
         return Response(status=status.HTTP_204_NO_CONTENT)
