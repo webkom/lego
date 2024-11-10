b2eece73cee9f8f72ddfdfef11466e918e1f5e50
Jonas de Luna Skulberg <jonasdls@gmail.com>
2024-11-10 13:36:19 +0100

    Update achievement score, make percentage cached

--- lego/apps/achievements/models.py
+++ lego/apps/achievements/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from lego.apps.users.models import User
+from lego.utils.decorators import abakus_cached_property
 from lego.utils.models import BasisModel
 
 from .constants import ACHIEVEMENT_IDENTIFIERS
@@ -25,6 +26,11 @@ class Achievement(BasisModel):
             .count()
         )
         return (achievement_users / total_users) * 100
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if hasattr(self.user, '_cached_properties') and 'achievement_score' in self.user.__dict__:
+            del self.user.__dict__['achievement_score']
 
     class Meta:
         constraints = [
--- lego/apps/achievements/utils/calculation_utils.py
+++ lego/apps/achievements/utils/calculation_utils.py
@@ -1,24 +1,23 @@
 
 import math
 
+from lego.apps.achievements.constants import EVENT_IDENTIFIER, EVENT_PRICE_IDENTIFIER, EVENT_RANK_IDENTIFIER, MEETING_IDENTIFIER, PENALTY_IDENTIFIER, POLL_IDENTIFIER, QUOTE_IDENTIFIER
+
 ACHIEVEMENT_DATA = {
-    "event_count": [0, 1, 2, 3, 4, 6], 
-    "event_rank": [7, 8, 9],                
-    "quote_count": [2],                      
-    "event_price": [2, 3, 5],                
-    "meeting_hidden": [2],                  
-    "poll_count": [0, 2, 4],                
-    "penalty_period": [0, 3, 5, 6],         
+    EVENT_IDENTIFIER: [0, 1, 2, 3, 4, 6], 
+    EVENT_RANK_IDENTIFIER: [7, 8, 9],                
+    QUOTE_IDENTIFIER: [2],                      
+    EVENT_PRICE_IDENTIFIER: [2, 3, 5],                
+    MEETING_IDENTIFIER: [2],                  
+    POLL_IDENTIFIER: [0, 2, 4],                
+    PENALTY_IDENTIFIER: [0, 3, 5, 6],         
 }
 
 
-def calculate_user_rank(user):
-    alpha = 0.55
-    beta = 0.45
-    gamma = 0.35
+def calculate_user_rank(user, alpha=0.68, beta=0.45, gamma=0.43, w=0.75):
     N = len(ACHIEVEMENT_DATA)
     highest_rarity = 0
-    normalized_rarities_product = 1
+    weighted_rarities_product = 1
     achievement_count = user.achievements.count()
 
     if achievement_count == 0:
@@ -29,19 +28,25 @@ def calculate_user_rank(user):
         level = achievement.level
 
         rarity_list = ACHIEVEMENT_DATA.get(identifier, [])
-        
         rarity = rarity_list[level] if level < len(rarity_list) else 0
         
         highest_rarity = max(highest_rarity, rarity)
-        normalized_rarity = (rarity + 1) / 10
-        normalized_rarities_product *= normalized_rarity
+        
+        weighted_rarity = math.log(rarity + 2) ** 2
+        weighted_rarities_product *= weighted_rarity
 
-    G = math.pow(normalized_rarities_product, 1 / achievement_count)
+    G = math.pow(weighted_rarities_product, 1 / achievement_count)
+    
+    G_normalized = G / (math.log(9 + 2) ** 2)
+
+    baseline = (highest_rarity + 1) / 2
+
+    G_blended = w * G_normalized + (1 - w) * baseline / (math.log(9 + 2) ** 2)
 
     highest_rarity_component = alpha * (math.sqrt(highest_rarity + 1) / math.sqrt(10))
-    geometric_mean_component = beta * G
+    geometric_mean_component = beta * G_blended
     achievement_count_component = gamma * (math.log(achievement_count + 1) / math.log(N + 1))
 
-    rank = (4 * (highest_rarity_component + geometric_mean_component + achievement_count_component)) / (alpha + beta + gamma)
-
-    return rank
+    rank = (100 * (highest_rarity_component + geometric_mean_component + achievement_count_component)) / (alpha + beta + gamma)
+    rounded = round(rank, 2)
+    return rounded
