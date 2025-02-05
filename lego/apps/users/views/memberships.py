5e4c0ec3b056b42966e301586b4e6e6ea6e4faab
sondrejk <sondrejk@gmail.com>
2025-02-05 21:16:16 +0100

    Prevent non-abakus members from joining interest groups
    Co-authored-by: Tinde <tindewa.31@gmail.com>

--- lego/apps/users/views/memberships.py
+++ lego/apps/users/views/memberships.py
@@ -1,8 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, viewsets
+from rest_framework import filters, status, viewsets
+from rest_framework.response import Response
 
 from lego.apps.permissions.api.filters import LegoPermissionFilter
 from lego.apps.permissions.api.views import AllowedPermissionsMixin
+from lego.apps.users import constants
 from lego.apps.users.filters import MembershipFilterSet
 from lego.apps.users.models import AbakusGroup, Membership
 from lego.apps.users.serializers.memberships import MembershipSerializer
@@ -32,4 +34,10 @@ class MembershipViewSet(AllowedPermissionsMixin, viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         request.data["abakus_group"] = kwargs["group_pk"]
+        group_id = kwargs["group_pk"]
+        group = AbakusGroup.objects.get(pk=group_id)
+        
+        if group.type == constants.GROUP_INTEREST and not request.user.has_grade_group:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
         return super(MembershipViewSet, self).create(request, *args, **kwargs)
