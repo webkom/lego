c2b1569b5a6ec8c1bb37ced8aa7634f728e959f4
Jonas de Luna Skulberg <113468143+jonasdeluna@users.noreply.github.com>
2025-02-12 21:46:12 +0100

    Fix delete being bugged due to 409 and constraints

--- lego/apps/achievements/views.py
+++ lego/apps/achievements/views.py
@@ -1,7 +1,16 @@
-from rest_framework import mixins, permissions, viewsets
+from rest_framework import mixins, permissions, status, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from lego.apps.achievements.constants import (
+    EVENT_RULES,
+    EVENT_RULES_IDENTIFIER,
+    KEYPRESS_ORDER,
+    KEYPRESS_ORDER_IDENTIFIER,
+)
+from lego.apps.achievements.models import Achievement
 from lego.apps.achievements.pagination import AchievementLeaderboardPagination
+from lego.apps.achievements.serializers import KeypressOrderSerializer
 from lego.apps.users.models import User
 from lego.apps.users.serializers.users import PublicUserWithGroupsSerializer
 
@@ -26,3 +35,36 @@ class LeaderBoardViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
             return self.get_paginated_response(serializer.data)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class AchievementViewSet(viewsets.GenericViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    serializer_class = KeypressOrderSerializer
+
+    @action(detail=False, methods=["POST"])
+    def getting_wood(self, request, *args, **kwargs):
+        achievement, created = Achievement.objects.get_or_create(
+            identifier=EVENT_RULES[EVENT_RULES_IDENTIFIER]["identifier"],
+            user=request.user,
+            level=0,
+        )
+        if created:
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False, methods=["POST"])
+    def keypress_order(self, request, *args, **kwargs):
+        code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13]
+        if request.data.get("code", []) == code:
+            achievement, created = Achievement.objects.get_or_create(
+                identifier=KEYPRESS_ORDER[KEYPRESS_ORDER_IDENTIFIER]["identifier"],
+                user=request.user,
+                level=0,
+            )
+            if created:
+                return Response(status=status.HTTP_201_CREATED)
+            else:
+                return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
