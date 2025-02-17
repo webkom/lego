34631004d7b08078a4dedce71f40cfa61c3f6533
Christoffer Nguyen <113469446+ch0rizo@users.noreply.github.com>
2025-02-17 16:32:43 +0100

    Feature - Delete Membership history (#3737)
    * Add: method to delete membership history

--- lego/apps/users/views/membership_history.py
+++ lego/apps/users/views/membership_history.py
@@ -1,9 +1,10 @@
-from rest_framework import mixins, permissions, viewsets
+from rest_framework import mixins, permissions, status, viewsets
+from rest_framework.response import Response
 
 from lego.apps.permissions.constants import EDIT
-from lego.apps.users.constants import PUBLIC_GROUPS
+from lego.apps.users.constants import GROUP_INTEREST, PUBLIC_GROUPS
 from lego.apps.users.filters import MembershipHistoryFilterSet
-from lego.apps.users.models import Membership, MembershipHistory
+from lego.apps.users.models import AbakusGroup, Membership, MembershipHistory
 from lego.apps.users.serializers.membership_history import MembershipHistorySerializer
 
 
@@ -27,3 +28,25 @@ class MembershipHistoryViewSet(
             return queryset.filter(abakus_group__type__in=PUBLIC_GROUPS)
 
         return queryset
+
+    def delete(self, request):
+        try:
+            group = AbakusGroup.objects.get(id=request.data["group_id"])
+            user_membership_history = MembershipHistory.objects.filter(
+                user__id=request.user.id, abakus_group__id=group.id
+            )
+
+            if group.type == GROUP_INTEREST and len(user_membership_history) != 0:
+                name = user_membership_history[0].abakus_group.name
+                user_membership_history.delete()
+                return Response(
+                    {"result": f"{name} got deleted"}, status=status.HTTP_200_OK
+                )
+            return Response(
+                {"result": "Nothing to delete"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        except Exception as e:
+            return Response(
+                {"result": f"Unexpected error: {str(e)}"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
