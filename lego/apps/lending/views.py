56e8cf093d06a183451facde0064041bdd0f8eec
Jonas de Luna Skulberg <jonasdls@gmail.com>
2025-03-05 20:17:19 +0100

    Add pagination to admin lending request view

--- lego/apps/lending/views.py
+++ lego/apps/lending/views.py
@@ -75,5 +75,9 @@ class LendingRequestViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=["get"], url_path="admin")
     def admin(self, request, *args, **kwargs):
         queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
