00954e0d387072485b466503dad70a61204dce49
Christoffer Nguyen <christofferhoang@gmail.com>
2025-03-21 15:05:37 +0100

    add a responsible group

--- lego/apps/lending/serializers.py
+++ lego/apps/lending/serializers.py
@@ -9,6 +9,8 @@ from lego.apps.lending.constants import (
     LENDING_REQUEST_TRANSLATION_MAP,
 )
 from lego.apps.lending.models import LendableObject, LendingRequest
+from lego.apps.users.fields import AbakusGroupField
+from lego.apps.users.models import AbakusGroup
 from lego.apps.users.serializers.users import PublicUserSerializer
 from lego.utils.serializers import (
     BasisModelSerializer,
@@ -19,10 +21,13 @@ from lego.utils.serializers import (
 class LendableObjectSerializer(BasisModelSerializer):
     image = ImageField(required=False, options={"height": 500})
     can_lend = serializers.SerializerMethodField()
+    can_edit_groups = AbakusGroupField(
+        read_only=True, many=True
+    )
 
     class Meta:
         model = LendableObject
-        fields = ("id", "title", "description", "image", "location", "can_lend")
+        fields = ("id", "title", "description", "image", "can_edit_groups", "location", "can_lend")
 
     def get_can_lend(self, obj):
         return obj.can_lend(self.context["request"].user)
