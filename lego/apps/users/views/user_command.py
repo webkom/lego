f320145857b010cb263ee42a71b5c620b5a82da3
Christoffer Nguyen <christofferhoang@gmail.com>
2025-10-07 19:46:02 +0200

    no backend validation

--- lego/apps/users/views/user_command.py
+++ lego/apps/users/views/user_command.py
@@ -3,8 +3,6 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from lego.apps.users.validators import validate_command_id
-
 
 class UserCommandViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
@@ -16,13 +14,6 @@ class UserCommandViewSet(viewsets.ViewSet):
         if not command_id:
             return Response(
                 {"error": "Missing command_id"}, status=status.HTTP_400_BAD_REQUEST
-            )
-
-        try:
-            validate_command_id(command_id)
-        except Exception:
-            return Response(
-                {"error": "Invalid command_id"}, status=status.HTTP_400_BAD_REQUEST
             )
 
         cmds = request.user.record_command_usage(command_id)
