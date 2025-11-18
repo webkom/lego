4201b030208ba139966bb137f3823274c02386ac
Tony Ngo <tonyworep@gmail.com>
2025-11-18 20:31:43 +0100

    fix response and info field

--- lego/apps/users/views/christmas_slot.py
+++ lego/apps/users/views/christmas_slot.py
@@ -14,7 +14,7 @@ class ChristmasSlotViewSet(viewsets.ModelViewSet):
         info = data.get("info")
         answer = data.get("answer")
 
-        _, created = ChristmasSlot.objects.update_or_create(slot=slot, defaults={"info": info, "answer": answer})
+        _, created = ChristmasSlot.objects.update_or_create(slot=slot, defaults={"answer": answer})
 
         if created:
             return Response(f"Christmas slot {slot} created", status=status.HTTP_201_CREATED)
@@ -36,10 +36,10 @@ class ChristmasSlotUserViewSet(viewsets.ModelViewSet):
             
             for christmas_slot in self.get_queryset():
                 if christmas_slot.slot.slot == request.data.get("slot"):
-                    return Response(f"Christmas slot {request.data.get('slot')} already exists", status=status.HTTP_200_OK)
+                    return Response({"message": f"Christmas slot {request.data.get('slot')} already exists"}, status=status.HTTP_200_OK)
             
             ChristmasSlotUser.objects.create(slot=specific_slot, user=self.request.user)
-            return Response(f"User {self.request.user} created slot {specific_slot}", status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_201_CREATED)
         except:
             return Response(
                 {"error": "Christmas slot does not exist"},
