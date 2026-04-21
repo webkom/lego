3fb628a89f68a16f7de4b6aa6b9b1d9d528820ec
Viljen Apalset Vassbø <90694640+Viljen789@users.noreply.github.com>
2026-04-21 20:05:23 +0200

    Add new easter egg trophies for 2026

--- lego/apps/jwt/authentication.py
+++ lego/apps/jwt/authentication.py
@@ -1,7 +1,9 @@
-from django.contrib.auth.models import update_last_login
+from django.utils import timezone
 
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from structlog import get_logger
+
+from lego.apps.users.models import User
 
 log = get_logger()
 
@@ -17,8 +19,11 @@ class Authentication(JSONWebTokenAuthentication):
         if authentication:
             user = authentication[0]
             log.bind(current_user=user.id)
-            update_last_login(None, user)
+            current_time = timezone.now()
+            User.objects.filter(pk=user.pk).update(
+                last_login=current_time, inactive_notified_counter=0
+            )
+            user.last_login = current_time
             user.inactive_notified_counter = 0
-            user.save()
 
         return authentication
