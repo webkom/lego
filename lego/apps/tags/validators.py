53550bcf01ebf870221d87bb266c676c56a85559
Vebjorn Elvekrok <vebjorn.elvekrok@gmail.com>
2026-08-20 18:20:52 +0200

    Allow diacritics in tags

--- lego/apps/tags/validators.py
+++ lego/apps/tags/validators.py
@@ -1,7 +1,9 @@
 from django.core.validators import RegexValidator
 from django.utils.regex_helper import _lazy_re_compile
 
-slug_re = _lazy_re_compile(r"^[-a-zA-ZæøåÆØÅ0-9:]+\Z")
+slug_re = _lazy_re_compile(r"^(?:[^\W_]|[-:])+\Z")
 validate_tag = RegexValidator(
-    slug_re, "Enter a valid 'tag' consisting only of letters and numbers.", "invalid"
+    slug_re,
+    "Enter a valid 'tag' consisting only of letters, numbers, hyphens and colons.",
+    "invalid",
 )
