25e54cfd9a177e88e30d803448a4e516a30340e4
Isak Berg Endresen <isakbendresen@gmail.com>
2026-09-07 08:33:16 +0200

    Properly leave ws groups

--- lego/apps/websockets/consumers.py
+++ lego/apps/websockets/consumers.py
@@ -57,7 +57,9 @@ class GroupConsumer(JsonWebsocketConsumer):
             if group in self.user_groups:
                 AsyncToSync(self.channel_layer.group_discard)(group, self.channel_name)
                 self.user_groups.remove(group)
-                self.send_message(constants.WS_GROUP_LEAVE_SUCCESS)
+                self.send_message(
+                    constants.WS_GROUP_LEAVE_SUCCESS, payload={"group": group}
+                )
 
     def send_message(self, type: str, payload=None, meta=None):
         """
